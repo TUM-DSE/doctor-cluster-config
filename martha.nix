6c0e123be1813c29e4f3d477dd42335ac26d3ccd
Jörg Thalheim <joerg@thalheim.io>
2019-07-12 09:33:09 +0000

    update to nfs4 for better speed

--- martha.nix
+++ martha.nix
@@ -7,7 +7,7 @@
 
   fileSystems."/home" = {
     device = "192.168.2.1:/home";
-    fsType = "nfs";
+    fsType = "nfs4";
     options = [
       "noauto" "x-systemd.automount" "x-systemd.device-timeout=10" "timeo=14" "x-systemd.idle-timeout=1min"
     ];
