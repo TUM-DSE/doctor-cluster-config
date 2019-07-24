55cde774cf1027500e601c8b72e3aa46d080b3bf
Jörg Thalheim <joerg@thalheim.io>
2019-07-24 14:17:32 +0000

    dpdk: chmod pci device resources for unprivileged access

--- modules/dpdk.nix
+++ modules/dpdk.nix
@@ -1,4 +1,4 @@
-{ config, ... }: {
+{ config, pkgs, ... }: {
   boot.kernelParams = [
     # spdk/dpdk hugepages
     "default_hugepagesz=1G" "hugepagesz=1G" "hugepages=16"
@@ -7,4 +7,15 @@
     config.boot.kernelPackages.dpdk.kmod
   ];
   boot.kernelModules = [ "igb_uio" ];
+
+  # From the output of udevadm info -a -p /sys/bus/pci/devices/0000:03:00.0,
+  # where 0000:03:00.0 is our NVME device
+  services.udev.extraRules = ''
+    # Intel Corporation Express Flash NVMe P4500/P4600
+    ATTR{vendor}=="0x8086" ATTR{device}=="0x0a54", RUN+="${pkgs.coreutils}/bin/chmod 666 /sys/bus/pci/devices/$kernel/resource0"
+
+    # Intel Corporation Ethernet Controller XL710 for 40GbE QSFP+
+     ATTR{vendor}=="0x8086" ATTR{device}=="0x1583", RUN+="${pkgs.coreutils}/bin/chmod 666 /sys/bus/pci/devices/$kernel/resource0"
+     ATTR{vendor}=="0x8086" ATTR{device}=="0x1583", RUN+="${pkgs.coreutils}/bin/chmod 666 /sys/bus/pci/devices/$kernel/resource3"
+  '';
 }
