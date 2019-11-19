3cbc1f56122873cf522b31467eb328e8aba8c3ad
Jörg Thalheim <joerg@thalheim.io>
2019-11-19 17:23:55 +0000

    amy & clara: correct tinc addresses

--- amy.nix
+++ amy.nix
@@ -6,6 +6,6 @@
   networking.hostName = "amy";
   networking.retiolum = {
     ipv4 = "10.243.29.181";
-    ipv6 = "42:0:3c46:7f2f:ed62:c6fd:9f1b:9901";
+    ipv6 = "42:0:3c46:1551:1906:bc7c:801f:3c4";
   };
 }
--- clara.nix
+++ clara.nix
@@ -5,7 +5,7 @@
 
   networking.hostName = "clara";
   networking.retiolum = {
-    ipv4 = "10.243.29.180";
-    ipv6 = "42:0:3c46:7f2f:ed62:c6fd:9f1b:9901";
+    ipv4 = "10.243.29.182";
+    ipv6 = "42:0:3c46:e4e9:7814:459b:3f23:f455";
   };
 }
