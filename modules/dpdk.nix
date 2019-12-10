1c9c92db4270cb9f6d1ce5b700c6b7ca5461f178
Jörg Thalheim <joerg@thalheim.io>
2019-12-10 07:55:28 +0000

    dpdk: switch to 2 MB hugepages & make it more configurable per server

--- modules/dpdk.nix
+++ modules/dpdk.nix
@@ -1,21 +1,43 @@
-{ config, pkgs, ... }: {
-  boot.kernelParams = [
-    # spdk/dpdk hugepages
-    "default_hugepagesz=1G" "hugepagesz=1G" "hugepages=16"
-  ];
-  boot.extraModulePackages = [
-    config.boot.kernelPackages.dpdk.kmod
-  ];
-  boot.kernelModules = [ "igb_uio" ];
+{ config, pkgs, lib, ... }:
+with lib;
+{
+  options = {
+    boot.hugepages.size = mkOption {
+      type = types.enum [ "1GB" "2MB" ];
+      description = ''
+        Size of one hugetable
+      '';
+      default = "2MB";
+    };
+    boot.hugepages.number = mkOption {
+      type = types.int;
+      description = ''
+        own ipv6 address
+      '';
+      default = 1000;
+    };
+  };
+  config = {
+    boot.kernelParams = [
+      # spdk/dpdk hugepages
+      "default_hugepagesz=${config.boot.hugepages.size}"
+      "hugepagesz=${config.boot.hugepages.size}"
+      "hugepages=${toString config.boot.hugepages.number}"
+    ];
+    boot.extraModulePackages = [
+      config.boot.kernelPackages.dpdk.kmod
+    ];
+    boot.kernelModules = [ "igb_uio" ];
 
-  # From the output of udevadm info -a -p /sys/bus/pci/devices/0000:03:00.0,
-  # where 0000:03:00.0 is our NVME device
-  services.udev.extraRules = ''
-    # Intel Corporation Express Flash NVMe P4500/P4600
-    ATTR{vendor}=="0x8086" ATTR{device}=="0x0a54", RUN+="${pkgs.coreutils}/bin/chmod 666 /sys/bus/pci/devices/$kernel/resource0"
+    # From the output of udevadm info -a -p /sys/bus/pci/devices/0000:03:00.0,
+    # where 0000:03:00.0 is our NVME device
+    services.udev.extraRules = ''
+      # Intel Corporation Express Flash NVMe P4500/P4600
+      ATTR{vendor}=="0x8086" ATTR{device}=="0x0a54", RUN+="${pkgs.coreutils}/bin/chmod 666 /sys/bus/pci/devices/$kernel/resource0"
 
-    # Intel Corporation Ethernet Controller XL710 for 40GbE QSFP+
-    ATTR{vendor}=="0x8086" ATTR{device}=="0x1583", RUN+="${pkgs.coreutils}/bin/chmod 666 /sys/bus/pci/devices/$kernel/resource0"
-    ATTR{vendor}=="0x8086" ATTR{device}=="0x1583", RUN+="${pkgs.coreutils}/bin/chmod 666 /sys/bus/pci/devices/$kernel/resource3"
-  '';
+      # Intel Corporation Ethernet Controller XL710 for 40GbE QSFP+
+      ATTR{vendor}=="0x8086" ATTR{device}=="0x1583", RUN+="${pkgs.coreutils}/bin/chmod 666 /sys/bus/pci/devices/$kernel/resource0"
+      ATTR{vendor}=="0x8086" ATTR{device}=="0x1583", RUN+="${pkgs.coreutils}/bin/chmod 666 /sys/bus/pci/devices/$kernel/resource3"
+    '';
+  };
 }
