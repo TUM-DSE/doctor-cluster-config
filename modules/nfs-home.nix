8f0ac90a997752905b8438a8a47d8a3eb0e0cdca
Jörg Thalheim <joerg@thalheim.io>
2020-02-13 12:11:38 +0000

    nfs-home: remove hard-coded ip address

--- modules/nfs-home.nix
+++ modules/nfs-home.nix
@@ -1,6 +1,6 @@
-{ ... }: {
+{ config, ... }: {
   fileSystems."/home" = {
-    device = "129.215.165.52:/home";
+    device = "${config.networking.doctorwho.hosts.eve.ipv4}:/home";
     fsType = "nfs4";
     options = [
       "noauto" "x-systemd.automount" "x-systemd.device-timeout=10" "timeo=14" "x-systemd.idle-timeout=1min"
