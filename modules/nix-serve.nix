4714811063293525ef55f0e6269be9f0ebeb9fd7
Jörg Thalheim <joerg@thalheim.io>
2020-06-15 17:49:32 +0000

    nix-serve: disable https

--- modules/nix-serve.nix
+++ modules/nix-serve.nix
@@ -8,7 +8,7 @@
   nix.binaryCaches = let
     filterHosts = name: host: name != config.networking.hostName && host.serveBinaryCache;
     filteredHosts = lib.filterAttrs filterHosts config.networking.doctorwho.hosts;
-  in lib.mapAttrsToList (name: host: "https://${name}:5000") filteredHosts;
+  in lib.mapAttrsToList (name: host: "http://${name}:5000") filteredHosts;
 
   nix.binaryCachePublicKeys = [
     "doctor-who-cache.thalheim.io:2kOtVHXVpm7MtlWA7o4VwV35lgAJ7oF9b6xJE23Kfd4="
