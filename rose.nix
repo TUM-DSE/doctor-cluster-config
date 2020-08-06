05299057900e296f44f8390a348152b6f9b68693
Jörg Thalheim <joerg@thalheim.io>
2020-08-06 14:33:09 +0000

    move nfs to amy

--- rose.nix
+++ rose.nix
@@ -1,75 +1,12 @@
 { pkgs, lib, config, ...}: {
+  imports = [
+    ./modules/nfs-home.nix
+  ];
+
   networking.hostName = "rose";
   networking.retiolum = {
     ipv4 = "10.243.29.178";
     ipv6 = "42:0:3c46:a5a1:618d:663a:f0f1:ec88";
   };
 
-  services.nfs.server.enable = true;
-  services.nfs.server.exports = ''
-    /home/ ${lib.concatMapStringsSep " " (host:
-      ''${host.ipv4}(rw,nohide,insecure,no_subtree_check,no_root_squash)'')
-      (lib.attrValues config.networking.doctorwho.hosts)}
-  '';
-
-  services.borgbackup.jobs.joerg = {
-    paths = [
-      "/home/joerg"
-    ];
-    doInit = true;
-    repo = "borgbackup@eddie.thalheim.io:rose";
-    postHook = ''
-      if [[ "$exitStatus" == "0" ]]; then
-        ${pkgs.curl}/bin/curl -XPOST -fsS --retry 3 https://hc-ping.com/6b08af9e-aee6-4efc-a059-d009e244afbb
-      else
-        ${pkgs.curl}/bin/curl -XPOST -fsS --retry 3 https://hc-ping.com/6b08af9e-aee6-4efc-a059-d009e244afbb/fail
-      fi
-    '';
-    encryption.mode = "none";
-    compression = "auto,zstd";
-    startAt = "daily";
-    prune.keep = {
-      within = "1d"; # Keep all archives from the last day
-      daily = 7;
-      weekly = 4;
-      monthly = 0;
-    };
-  };
-
-  services.borgbackup.jobs.all-homes = {
-    paths = [
-      "/home"
-    ];
-    doInit = true;
-    repo = "/mnt/backup/borgbackup";
-    preHook = ''
-      ${pkgs.sshfs}/bin/sshfs -oIdentityFile=/etc/nixos/secrets/borgbackup-ssh-key -oPort=22222 s1443541@csce.datastore.ed.ac.uk:/csce/datastore/inf/users/s1443541 /mnt/backup
-    '';
-    postHook = ''
-      if [[ "$exitStatus" == "0" ]]; then
-        ${pkgs.curl}/bin/curl -XPOST -fsS --retry 3 https://hc-ping.com/8fdc97f7-1c32-4829-9dd2-f78e01086f41
-      else
-        ${pkgs.curl}/bin/curl -XPOST -fsS --retry 3 https://hc-ping.com/8fdc97f7-1c32-4829-9dd2-f78e01086f41/fail
-      fi
-    '';
-    encryption = {
-      mode = "repokey";
-      passCommand = "cat /etc/nixos/secrets/borgbackup-password";
-    };
-    compression = "auto,zstd";
-    startAt = "daily";
-    prune.keep = {
-      within = "1d"; # Keep all archives from the last day
-      daily = 7;
-      weekly = 4;
-      monthly = 0;
-    };
-  };
-  # hide sshfs from the system
-  systemd.services.borgbackup-job-all-homes.serviceConfig.PrivateMounts = true;
-
-  fileSystems."/home" = {
-    device = "zroot/root/home";
-    fsType = "zfs";
-  };
 }
