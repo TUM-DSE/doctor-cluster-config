59ea0127c3de59ec947620ed919e2c2c7a8a94b7
Jörg Thalheim <joerg@thalheim.io>
2020-10-19 15:51:21 +0200

    rename yaz -> doctor

--- doctor.nix
+++ doctor.nix
@@ -13,6 +13,13 @@
     ./modules/r_reset.nix
   ];
 
+  networking.hostName = "doctor";
+  networking.retiolum = {
+    ipv4 = "10.243.29.181";
+    ipv6 = "42:0:3c46:96e2:72f4:be89:d6eb:ab14";
+  };
+  services.openssh.extraConfig = "HostCertificate ${./secrets/certs/doctor-cert.pub}";
+
   # port: 1/A/D14/1/14
   # mac address:
 }
