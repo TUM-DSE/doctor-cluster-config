1a37d927e2b9d30698f8dc21c0ca680979fe3035
Jörg Thalheim <joerg@thalheim.io>
2020-10-22 11:31:31 +0000

    doctor: fix retiolum address

--- doctor.nix
+++ doctor.nix
@@ -15,7 +15,7 @@
 
   networking.hostName = "doctor";
   networking.retiolum = {
-    ipv4 = "10.243.29.181";
+    ipv4 = "10.243.29.186";
     ipv6 = "42:0:3c46:96e2:72f4:be89:d6eb:ab14";
   };
   services.openssh.extraConfig = "HostCertificate ${./secrets/certs/doctor-cert.pub}";
