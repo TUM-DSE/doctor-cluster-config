bcc1335e3508b0b080909ae5c4f9df2f5abfcc4f
Jörg Thalheim <joerg@thalheim.io>
2020-10-22 11:46:16 +0000

    doctor: add more uart logging

--- modules/raspberrypi-4.nix
+++ modules/raspberrypi-4.nix
@@ -14,12 +14,17 @@
   boot.loader.grub.enable = false;
   boot.loader.raspberryPi.enable = true;
   boot.loader.raspberryPi.version = 4;
+  # get more log messages from bootloader over uart console
+  boot.loader.raspberryPi.firmwareConfig = ''
+    uart_2ndstage=1
+  '';
 
   # Mainline doesn't work yet
   boot.kernelPackages = pkgs.linuxPackages_rpi4;
 
   # ttyAMA0 is the serial console broken out to the GPIO
   boot.kernelParams = [
+    "earlycon=uart8250,mmio32,0xfe215040"
     "console=ttyAMA0,115200"
     "console=tty1"
   ];
