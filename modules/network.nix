0b237f466cb834dc59c57f5e0c8d648e046cd7a8
Jörg Thalheim <joerg@thalheim.io>
2020-11-29 07:59:07 +0000

    systemd-networkd: don't restart on upgrade

--- modules/network.nix
+++ modules/network.nix
@@ -7,6 +7,8 @@
 
   # often hangs
   systemd.services.systemd-networkd-wait-online.enable = false;
+  # sometimes cannot be restarted -> breaks system upgrade
+  systemd.services.systemd-networkd.restartIfChanged = false;
 
   # add an entry to /etc/hosts for each host
   networking.extraHosts = lib.concatStringsSep "\n" (lib.mapAttrsToList
