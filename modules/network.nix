061024bc2753ae101636634c4d7700ddad4ff8d1
Jörg Thalheim <joerg@thalheim.io>
2020-11-29 08:09:56 +0000

    firewall: don't restart on upgrade

--- modules/network.nix
+++ modules/network.nix
@@ -10,7 +10,7 @@
   # sometimes cannot be restarted -> breaks system upgrade
   systemd.services.systemd-networkd.restartIfChanged = false;
   # fails to delete some chain on upgrade...
-  systemc.services.firewall.restartIfChanged = false
+  systemd.services.firewall.restartIfChanged = false;
 
   # add an entry to /etc/hosts for each host
   networking.extraHosts = lib.concatStringsSep "\n" (lib.mapAttrsToList
