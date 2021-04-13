c779ea30f14bbc691699334ca11ae3e30e52d461
Jörg Thalheim <joerg@thalheim.io>
2021-04-13 07:08:18 +0000

    containerd: apply kata-deploy configuration

--- modules/k3s/default.nix
+++ modules/k3s/default.nix
@@ -28,6 +28,32 @@ in {
     [plugins.cri.cni]
       bin_dir = "${pkgs.cni-plugins}/bin"
       conf_dir = "${pkgs.writeTextDir "net.d/10-flannel.conflist" flannel}/net.d"
+
+    # comes from kata-deploy
+    [plugins.cri.containerd.runtimes.kata]
+      runtime_type = "io.containerd.kata.v2"
+      privileged_without_host_devices = true
+      pod_annotations = ["io.katacontainers.*"]
+      [plugins.cri.containerd.runtimes.kata.options]
+        ConfigPath = "/opt/kata/share/defaults/kata-containers/configuration.toml"
+    [plugins.cri.containerd.runtimes.kata-fc]
+      runtime_type = "io.containerd.kata-fc.v2"
+      privileged_without_host_devices = true
+      pod_annotations = ["io.katacontainers.*"]
+      [plugins.cri.containerd.runtimes.kata-fc.options]
+        ConfigPath = "/opt/kata/share/defaults/kata-containers/configuration-fc.toml"
+    [plugins.cri.containerd.runtimes.kata-qemu]
+      runtime_type = "io.containerd.kata-qemu.v2"
+      privileged_without_host_devices = true
+      pod_annotations = ["io.katacontainers.*"]
+      [plugins.cri.containerd.runtimes.kata-qemu.options]
+        ConfigPath = "/opt/kata/share/defaults/kata-containers/configuration-qemu.toml"
+    [plugins.cri.containerd.runtimes.kata-clh]
+      runtime_type = "io.containerd.kata-clh.v2"
+      privileged_without_host_devices = true
+      pod_annotations = ["io.katacontainers.*"]
+      [plugins.cri.containerd.runtimes.kata-clh.options]
+        ConfigPath = "/opt/kata/share/defaults/kata-containers/configuration-clh.toml"
   '';
   systemd.services.containerd.path = with pkgs; [ zfs ];
 
