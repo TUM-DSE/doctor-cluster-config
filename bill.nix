c40cb6e3a65507a369a9f7a4e3da9c2be99696f4
Jörg Thalheim <joerg@thalheim.io>
2021-04-28 16:18:21 +0000

    bill: fix tinc addresses

--- bill.nix
+++ bill.nix
@@ -7,8 +7,8 @@
 
   networking.hostName = "bill";
   networking.retiolum = {
-    ipv4 = "10.243.29.179";
-    ipv6 = "42:0:3c46:5466:7dbe:f27a:673f:ea64";
+    ipv4 = "10.243.29.195";
+    ipv6 = "42:0:3c46:87ed:6232:1016:7553:939c";
   };
 
   system.stateVersion = "20.09";
