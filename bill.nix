eb92d91dd2056a6f4dec684bed5401da7d46ce00
Jörg Thalheim <joerg@thalheim.io>
2021-04-29 16:12:07 +0000

    add mac-changer for bill

--- bill.nix
+++ bill.nix
@@ -12,5 +12,13 @@
     ipv6 = "42:0:3c46:87ed:6232:1016:7553:939c";
   };
 
+  systemd.network.links."00-fix-mac".extraConfig = ''
+    [Match]
+    MACAddress = 3c:ec:ef:2c:f5:55
+
+    [Link]
+    MACAddress = 00:e0:4c:68:00:8f
+  '';
+
   system.stateVersion = "20.09";
 }
