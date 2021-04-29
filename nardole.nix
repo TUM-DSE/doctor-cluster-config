11943af567ee6de71f0e5d4f0699435f13edeeb2
Jörg Thalheim <joerg@thalheim.io>
2021-04-29 16:11:29 +0000

    add mac-changer for nardole

--- nardole.nix
+++ nardole.nix
@@ -12,5 +12,13 @@
     ipv6 = "42:0:3c46:362d:a9aa:4996:c78e:839a";
   };
 
+  systemd.network.links."00-fix-mac".extraConfig = ''
+    [Match]
+    MACAddress = 3c:ec:ef:2c:f5:15
+
+    [Link]
+    MACAddress = 94:05:BB:11:2D:70
+  '';
+
   system.stateVersion = "20.09";
 }
