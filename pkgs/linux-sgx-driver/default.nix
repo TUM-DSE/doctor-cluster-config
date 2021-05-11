01a8d61aacd916c66acd8f24cbdf00294875f4f8
Jörg Thalheim <joerg@thalheim.io>
2021-05-11 13:50:22 +0000

    linux-sgx-driver: 2.6 -> 2.11

--- pkgs/linux-sgx-driver/default.nix
+++ pkgs/linux-sgx-driver/default.nix
@@ -4,20 +4,18 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "linux-sgx-driver";
-  version = "2.6";
+  version = "2.11";
 
   src = fetchFromGitHub {
     owner = "intel";
     repo = "linux-sgx-driver";
-    rev = "602374c738ca58f83a1c17574d08e5d5e6341953";
-    sha256 = "17zcx2p8m3m0b6w1r1sf11zznmlq8s4nqhxpc52crgvjaj946d0c";
+    rev = "sgx_driver_${version}";
+    sha256 = "sha256-zZ0FgCx63LCNmvQ909O27v/o4+93gefhgEE/oDr/bHw=";
   };
 
   makeFlags = [
     "KDIR=${kdir}"
   ];
-
-  patches = [ ./oot-metrics.patch ];
 
   nativeBuildInputs = [ perl ] ++ kernel.moduleBuildDependencies;
 
