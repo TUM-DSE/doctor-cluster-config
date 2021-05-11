29e5c29b86e25aaa3f6b079a661d9f05eeceac4f
Jörg Thalheim <joerg@thalheim.io>
2021-05-11 15:06:25 +0000

    upgrade kernel stuff

--- pkgs/graphene-sgx-driver/default.nix
+++ pkgs/graphene-sgx-driver/default.nix
@@ -1,4 +1,4 @@
-{ kernel, stdenv, fetchFromGitHub, python3, linux-sgx-driver }:
+{ kernel, stdenv, fetchFromGitHub, python3, isgx }:
 let
   kdir = "${kernel.dev}/lib/modules/${kernel.modDirVersion}/build";
 in
@@ -8,10 +8,10 @@ stdenv.mkDerivation rec {
     owner = "oscarlab";
     repo = "graphene-sgx-driver";
     rev = "4386dbeb1260d8743ea3a5fbe4fc5fd61322008c";
-    sha256 = "sha256-zZ0FgCx63LCNmvQ909O27v/o4+93gefhgEE/oDr/bHw=";
+    sha256 = "sha256-bhNtYFXQlJjm73n+De3hoT5Svvyb2r1E6KfmdNMV8Es=";
   };
 
-  ISGX_DRIVER_PATH = linux-sgx-driver.src;
+  ISGX_DRIVER_PATH = isgx.src;
 
   makeFlags = [
     "KDIR=${kdir}"
