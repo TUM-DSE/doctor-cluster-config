264ff587e9c3822bd3df70658dc3961530edbc29
Jörg Thalheim <joerg@thalheim.io>
2021-05-11 09:39:11 +0000

    telegraf: automatically update kubernetes token

--- modules/k3s/server.nix
+++ modules/k3s/server.nix
@@ -39,7 +39,7 @@ in
 
   services.telegraf = {
     extraConfig.kube_inventory = {
-      bearer_token = "/run/telegraf-kubernetes-token";
+      bearer_token = "/run/telegraf/kubernetes-token";
       url = "https://localhost:6443";
       insecure_skip_verify = true;
       resource_include = [ "pods" ];
@@ -47,8 +47,28 @@ in
   };
 
   systemd.tmpfiles.rules = [
-    "C /run/telegraf-kubernetes-token 400 telegraf root - /etc/nixos/secrets/telegraf-kubernetes-token"
+    "d /run/telegraf/ 0700 telegraf - -"
   ];
 
   systemd.services.k3s.serviceConfig.ExecStartPost = "${pkgs.k3s}/bin/k3s kubectl apply -f ${pkgs.writeText "resources.yaml" resources}";
+
+  systemd.services.telegraf-kubernetes-setup = {
+    wantedBy = [ "multi-user.target" ];
+    requires = ["k3s.service"];
+    after = ["k3s.service"];
+    script = ''
+      secretname=$(${pkgs.k3s}/bin/k3s kubectl get serviceaccount telegraf -o template="{{(index .secrets 0).name}}")
+      ${pkgs.k3s}/bin/k3s kubectl get secret "$secretname" -o template='{{.data.token}}' | base64 -d > /run/telegraf/kubernetes-token
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+    };
+  };
+
+  systemd.services.telegraf = {
+    requires = [ "telegraf-kubernetes-setup.service" ];
+    after = [ "telegraf-kubernetes-setup.service" ];
+  };
+
 }
