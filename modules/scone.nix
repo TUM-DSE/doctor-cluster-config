3c8ede23c8aa94ab03d1ef4ccaa02bfe6a866173
Jörg Thalheim <joerg@thalheim.io>
2021-05-12 19:15:08 +0000

    scone: remove localAttestation service

--- modules/scone.nix
+++ modules/scone.nix
@@ -3,22 +3,6 @@
     "C /root/.docker/config.json 400 root root - /etc/nixos/secrets/docker-config.json"
   ];
 
-  # Enable Local Attestation Service as a systemd service
-  virtualisation.oci-containers.containers.localAttestation = {
-    image = "sconecuratedimages/helloworld:las";
-    ports = [ "18766:18766" ];
-    extraOptions = [
-      "--device=/dev/isgx"
-      # exposes /var/run/aesmd/aesm.socket
-      "--volume=/var/run/aesmd:/var/run/aesmd"
-    ];
-  };
-
-  systemd.services.docker-localAttestation.serviceConfig = {
-    # does not stop on SIGTERM properly, dont' care since stateless anyway
-    TimeoutStopSec = lib.mkForce "1";
-  };
-
   # Open ports for communicating with CAS
   networking.firewall.allowedTCPPorts = [
     8081
