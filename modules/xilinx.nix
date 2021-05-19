dd644d3f4151258cbf77a0aa2237b63751014fb9
Jörg Thalheim <joerg@thalheim.io>
2021-05-19 08:30:18 +0000

    xrdp: rename xrdp to xilinx

--- modules/xilinx.nix
+++ modules/xilinx.nix
@@ -17,4 +17,12 @@ in {
   boot.extraModulePackages = [
     xrt-drivers
   ];
+
+  users.extraUsers.xilinx = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "docker" "plugdev" "input" ];
+    # /etc/nixos/secrets/xrdp-password
+    hashedPassword = "$6$JVnp0RXTKpCfSc.$A4mp4WmFzVFnwHUPYEjqMQmhlL/9o5tbKKv2y/nY3oojp/aLnWjUzTMspxCadcn4L9b7WyTFHt2VNVkZ6q9Y30";
+    uid = 2002;
+  };
 }
--- modules/xrdp.nix
+++ modules/xrdp.nix
@@ -8,10 +8,4 @@
     layout = "us";
     xkbVariant = "altgr-intl";
   };
-
-  users.extraUsers.xrdp = {
-    extraGroups = [ "wheel" "docker" "plugdev" "input" ];
-    # /etc/nixos/secrets/xrdp-password
-    hashedPassword = "$6$JVnp0RXTKpCfSc.$A4mp4WmFzVFnwHUPYEjqMQmhlL/9o5tbKKv2y/nY3oojp/aLnWjUzTMspxCadcn4L9b7WyTFHt2VNVkZ6q9Y30";
-  };
 }
