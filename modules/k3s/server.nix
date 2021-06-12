08195737b0d862589dff94a2ab6aafb14f3d1085
Jörg Thalheim <joerg@thalheim.io>
2021-06-12 07:58:04 +0000

    telegraf: fix token authentication for k3s

--- modules/k3s/server.nix
+++ modules/k3s/server.nix
@@ -8,6 +8,8 @@
   services.telegraf = {
     extraConfig.kube_inventory = {
       bearer_token = "/run/telegraf-kubernetes-token";
+      tls_cert = "/run/telegraf-kubernetes-cert";
+      tls_key = "/run/telegraf-kubernetes-key";
       url = "https://localhost:6443";
       insecure_skip_verify = true;
       resource_include = [ "pods" ];
@@ -20,7 +22,12 @@
     after = ["k3s.service"];
     serviceConfig = {
       Type = "oneshot";
-      ExecStart = "${pkgs.coreutils}/bin/install -o telegraf -m400 /etc/rancher/k3s/k3s.yaml /run/telegraf-kubernetes-token";
+      ExecStart = [
+        # we use the client/key for authentication and hence leave the kubernetes token empty
+        "${pkgs.coreutils}/bin/touch /run/telegraf-kubernetes-token"
+        "${pkgs.coreutils}/bin/install -o telegraf -m400 /var/lib/rancher/k3s/server/tls/client-admin.crt /run/telegraf-kubernetes-cert"
+        "${pkgs.coreutils}/bin/install -o telegraf -m400 /var/lib/rancher/k3s/server/tls/client-admin.key /run/telegraf-kubernetes-key"
+      ];
       # work-around potential race condition between k3s and this service.
       RestartSec = "1s";
       Restart = "on-failure";
@@ -29,7 +36,7 @@
   };
 
   systemd.services.telegraf = {
-    requires = [ "telegraf-kubernetes-setup.service" ];
+    wants = [ "telegraf-kubernetes-setup.service" ];
     after = [ "telegraf-kubernetes-setup.service" ];
   };
 }
