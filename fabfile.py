eed6ed00f0151863d562bcd543c1e650d0428728
Jörg Thalheim <joerg@thalheim.io>
2021-07-14 13:51:34 +0200

    fabfile: add reboot task

--- fabfile.py
+++ fabfile.py
@@ -56,6 +56,45 @@ def deploy_edinburgh(c):
     deploy_nixos(["amy.r", "martha.r", "clara.r", "donna.r", "sauron.r"])
 
 
+def wait_for_port(host: str, port: int, shutdown: bool=False) -> None:
+    import socket, time
+    while True:
+        try:
+            with socket.create_connection((host, port), timeout=1):
+                if shutdown:
+                    time.sleep(1)
+                    sys.stdout.write(".")
+                    sys.stdout.flush()
+                else:
+                    break
+        except OSError as ex:
+            if shutdown:
+                break
+            else:
+                time.sleep(0.01)
+                sys.stdout.write(".")
+                sys.stdout.flush()
+
+
+@task
+def reboot(c):
+    c.user = "root"
+    """
+    Reboot hosts. example usage: fab --hosts clara.r,donna.r reboot
+    """
+    c.run('reboot', asynchronous=True)
+
+    print(f"Wait for {c.host} to shutdown",end="")
+    sys.stdout.flush()
+    wait_for_port(c.host, c.port, shutdown=True)
+    print("")
+
+    print(f"Wait for {c.host} to start", end="")
+    sys.stdout.flush()
+    wait_for_port(c.host, c.port)
+    print("")
+
+
 @task
 def deploy_doctor(c):
     """
