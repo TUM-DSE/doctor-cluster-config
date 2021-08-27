15c219c0997965d2d263c6de5ad657154e9f22fb
Jörg Thalheim <joerg@thalheim.io>
2021-08-27 12:59:00 +0000

    xfstests: make fsgqa2/fsgqa normal users

--- modules/xfstest.nix
+++ modules/xfstest.nix
@@ -2,10 +2,11 @@
 
 {
   users.users.fsgqa.shell = "/bin/sh";
-  users.users.fsgqa.isSystemUser = true;
-  users.users.fsgqa2.isSystemUser = true;
+  users.users.fsgqa.isNormalUser = true;
   users.users."123456-fsgqa".isSystemUser = true;
   users.groups.fsgqa = {};
+  users.users.fsgqa2.isNormalUser = true;
+  users.users.fsgqa2.shell = "/bin/sh";
   users.users.fsgqa2.group = "fsgqa2";
   users.groups.fsgqa2 = {};
 }
