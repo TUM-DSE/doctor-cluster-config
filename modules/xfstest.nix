5c5fdf6af36271f2cb910d8f4caf22105df2314f
Jörg Thalheim <joerg@thalheim.io>
2021-08-27 14:49:09 +0200

    xfstest: fix up users

--- modules/xfstest.nix
+++ modules/xfstest.nix
@@ -1,8 +1,11 @@
 { config, lib, pkgs, ... }:
 
 {
+  users.users.fsgqa.shell = "/bin/sh";
   users.users.fsgqa.isSystemUser = true;
   users.users.fsgqa2.isSystemUser = true;
   users.users."123456-fsgqa".isSystemUser = true;
   users.groups.fsgqa = {};
+  users.users.fsgqa2.group = "fsgqa2";
+  users.groups.fsgqa2 = {};
 }
