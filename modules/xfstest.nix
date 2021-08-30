9b03fc964428e492a8565ce3bfc5fbcaa1fe3931
Jörg Thalheim <joerg@thalheim.io>
2021-08-30 16:16:19 +0200

    add daemon-user for xfstests

--- modules/xfstest.nix
+++ modules/xfstest.nix
@@ -3,10 +3,20 @@
 {
   users.users.fsgqa.shell = "/bin/sh";
   users.users.fsgqa.isNormalUser = true;
-  users.users."123456-fsgqa".isSystemUser = true;
+  users.users.fsgqa.group = "fsgqa";
   users.groups.fsgqa = {};
-  users.users.fsgqa2.isNormalUser = true;
+
+  users.users."123456-fsgqa".shell = "/bin/sh";
+  users.users."123456-fsgqa".isNormalUser = true;
+  users.users."123456-fsgqa".group = "fsgqa";
+  users.groups."123456-fsgqa" = {};
+
   users.users.fsgqa2.shell = "/bin/sh";
+  users.users.fsgqa2.isNormalUser = true;
   users.users.fsgqa2.group = "fsgqa2";
   users.groups.fsgqa2 = {};
+
+  users.users.daemon.isSystemUser = true;
+  users.users.daemon.group = "daemon";
+  users.groups.daemon = {};
 }
