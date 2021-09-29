e3354a227ca2359fbc732a3b9f9ef5bbfe6b4c1e
Jörg Thalheim <joerg@thalheim.io>
2021-09-29 19:17:12 +0000

    k3s: set explicit node-ip

--- modules/k3s/agent.nix
+++ modules/k3s/agent.nix
@@ -5,7 +5,6 @@
   services.k3s.role = "agent";
   # copied from server from bill:/var/lib/rancher/k3s/server/node-token
   services.k3s.tokenFile = "/etc/nixos/secrets/k3s-server-token";
-  # FIXME migrate once we got the right addresses for bill
   services.k3s.serverAddr = "https://bill.dse.in.tum.de:6443";
-  services.k3s.extraFlags = "--container-runtime-endpoint unix:///run/containerd/containerd.sock";
+  services.k3s.extraFlags = "--node-ip ${config.networking.doctorwho.currentHost.ipv4} --container-runtime-endpoint unix:///run/containerd/containerd.sock";
 }
