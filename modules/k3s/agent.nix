b8346b4e4a347455e485c0ed9f304c3bb84ff1cb
Jörg Thalheim <joerg@thalheim.io>
2021-10-03 11:52:27 +0200

    k3s: fix evaluation

--- modules/k3s/agent.nix
+++ modules/k3s/agent.nix
@@ -2,10 +2,10 @@
 {
   imports = [ ./. ];
 
-  sops.secrets.k3s-tokenfile = {};
+  sops.secrets.k3s-server-token = {};
   services.k3s.role = "agent";
   # copied from server from bill:/var/lib/rancher/k3s/server/node-token
-  services.k3s.tokenFile = config.sops.secrets.k3s-tokenfile.path;
+  services.k3s.tokenFile = config.sops.secrets.k3s-server-token.path;
   services.k3s.serverAddr = "https://bill.dse.in.tum.de:6443";
   services.k3s.extraFlags = "--node-ip ${config.networking.doctorwho.currentHost.ipv4} --container-runtime-endpoint unix:///run/containerd/containerd.sock";
 }
