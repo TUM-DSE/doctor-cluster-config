7561532fc0d6c506d0632f1dca4713e139d56709
Jörg Thalheim <joerg@thalheim.io>
2021-10-03 11:46:11 +0200

    eris: fix evaluation

--- modules/eris.nix
+++ modules/eris.nix
@@ -1,19 +1,20 @@
 { config, lib, pkgs, ... }:
 {
-  sops.secrets.cache-priv-key-pem.owner = "eris";
+  sops.secrets.cache-priv-key-pem = {};
   systemd.services.eris-git.serviceConfig = {
     BindReadOnlyPaths = lib.mkForce [
       "/etc/eris"
       "/etc/nixos/secrets"
     ];
     User = "eris";
+    ExecStartPre = "+${pkgs.coreutils}/bin/install -o eris -m400 ${config.sops.secrets.cache-priv-key-pem.path} /run/cache-priv-key.pem";
   };
   services.eris-git.enable = true;
   services.eris-git.configFile = toString (pkgs.writeText "eris.conf" ''
     {
       signing => {
         host    => 'doctor-who-cache.thalheim.io',
-        private => '${config.sops.secrets.cache-priv-key-pem.path}',
+        private => '/run/cache-priv-key.pem',
       },
       listen => [
         'http://[::]:5001',
