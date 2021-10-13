b9457ecaf74a4e5e7a93e6433b6162854ae81283
Jörg Thalheim <joerg@thalheim.io>
2021-10-13 19:30:11 +0200

    spoof macs for graham/ryan

--- hosts/graham.nix
+++ hosts/graham.nix
@@ -8,5 +8,7 @@
     ipv6 = "42:0:3c46:1551:1906:bc7c:801f:3c4";
   };
 
+  networking.interfaces.eno8303.macAddress = "3c:ec:ef:2c:f5:55";
+
   system.stateVersion = "21.05";
 }
--- hosts/ryan.nix
+++ hosts/ryan.nix
@@ -8,5 +8,7 @@
     ipv6 = "42:0:3c46:ab64:fa7c:171a:c94a:ac19";
   };
 
+  networking.interfaces.eno8303.macAddress = "3c:ec:ef:2c:f5:15";
+
   system.stateVersion = "21.05";
 }
