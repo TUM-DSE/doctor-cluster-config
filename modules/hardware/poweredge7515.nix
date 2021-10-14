d3c6e73233c14d52afd630712e67584f8eb4d5eb
Jörg Thalheim <joerg@thalheim.io>
2021-10-14 14:11:27 +0200

    poweredge: enable ipmi

--- modules/hardware/poweredge7515.nix
+++ modules/hardware/poweredge7515.nix
@@ -4,9 +4,10 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+    ../ipmi.nix
+  ];
 
   boot.initrd.availableKernelModules = [ "megaraid_sas" "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
