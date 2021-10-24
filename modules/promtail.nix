62e994a127cffa12b60c3b0e93151ffcbf3607e4
Jörg Thalheim <joerg@thalheim.io>
2021-10-24 19:48:43 +0200

    promtail: update loki address

--- modules/promtail.nix
+++ modules/promtail.nix
@@ -10,7 +10,7 @@
       clients = [{
         basic_auth.username = "promtail-doctor@thalheim.io";
         basic_auth.password_file = config.sops.secrets.promtail-password.path;
-        url = "http://rock.r/loki/api/v1/push";
+        url = "http://loki.r/loki/api/v1/push";
       }];
 
       scrape_configs = [{
