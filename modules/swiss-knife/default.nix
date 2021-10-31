e1eb6fd77e4ab32bd8e34194af08a8828d4fb095
Jörg Thalheim <joerg@thalheim.io>
2021-10-31 08:45:30 +0100

    swiss-knife: re-enable firewall

--- modules/swiss-knife/default.nix
+++ modules/swiss-knife/default.nix
@@ -7,6 +7,9 @@
     ./storage.nix
   ];
 
+  # Students are not careful exposing ports with containers
+  networking.firewall.enable = lib.mkForce true;
+
   # needed for the container task
   environment.systemPackages = [
     pkgs.docker-compose
