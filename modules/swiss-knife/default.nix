711fe14fabc6b952f9bc4c18c460ec7924a8225f
Jörg Thalheim <joerg@thalheim.io>
2021-11-01 13:21:31 +0100

    swiss-knife: also install podman as docker alternative

--- modules/swiss-knife/default.nix
+++ modules/swiss-knife/default.nix
@@ -10,6 +10,9 @@
   # Students are not careful exposing ports with containers
   networking.firewall.enable = lib.mkForce true;
 
+  # isolates better than docker
+  virtualisation.podman.enable = true;
+
   # needed for the container task
   environment.systemPackages = [
     pkgs.docker-compose
