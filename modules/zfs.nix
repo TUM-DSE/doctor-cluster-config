362efc721da2063398f08661f0fc3df13295fe91
Jörg Thalheim <joerg@thalheim.io>
2021-11-08 13:41:38 +0100

    zfs: disable io scheduler

--- modules/zfs.nix
+++ modules/zfs.nix
@@ -7,4 +7,9 @@
   # 5.12 is too new
   boot.zfs.enableUnstable = true;
   networking.hostId = "8425e348";
+
+  # ZFS already has its own scheduler. Without this my(@Artturin) computer froze for a second when i nix build something.
+  services.udev.extraRules = ''
+    ACTION=="add|change", KERNEL=="sd[a-z]*[0-9]*|mmcblk[0-9]*p[0-9]*|nvme[0-9]*n[0-9]*p[0-9]*", ENV{ID_FS_TYPE}=="zfs_member", ATTR{../queue/scheduler}="none"
+  '';
 }
