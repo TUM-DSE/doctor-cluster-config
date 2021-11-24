12ca9de081d00ff67a9aa09e6d315340b3b6e1e7
Jörg Thalheim <joerg@thalheim.io>
2021-11-24 11:05:01 +0100

    add update-prefetch service

--- modules/nix-daemon.nix
+++ modules/nix-daemon.nix
@@ -8,6 +8,9 @@
     # should be enough?
     nrBuildUsers = lib.mkDefault 32;
 
+    daemonIOSchedClass = "idle";
+    daemonCPUSchedPolicy = "idle";
+
     # https://github.com/NixOS/nix/issues/719
     extraOptions = ''
       builders-use-substitutes = true
@@ -29,6 +32,17 @@
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "mic92.cachix.org-1:gi8IhgiT3CYZnJsaW7fxznzTkMUOn1RY4GmXdT/nXYQ="
     ];
+  };
+
+  systemd.services.update-prefetch = {
+    wantedBy = [ "multi-user.target" ];
+    path = [ config.nix.package pkgs.nettools ];
+    script = ''
+      nix build \
+       --out-link /run/next-system \
+       github:Mic92/doctor-cluster-config/last-build#nixosConfigurations.$(hostname).config.system.build.toplevel
+    '';
+    startAt = "daily";
   };
 
   imports = [ ./builder.nix ];
