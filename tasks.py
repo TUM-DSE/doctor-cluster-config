1ae5db2e70ec9397ce8650c2fb7e4e9300db9df7
Jörg Thalheim <joerg@thalheim.io>
2021-12-20 17:53:53 +0100

    automate formatting disks

--- tasks.py
+++ tasks.py
@@ -2,9 +2,15 @@
 
 from invoke import task
 
+import subprocess
 import sys
-from typing import List
+import json
+from typing import List, Any
 from deploy_nixos import DeployHost, DeployGroup
+
+
+def get_hosts(hosts: str) -> List[DeployHost]:
+    return [DeployHost(h) for h in hosts.split(",")]
 
 
 def deploy_nixos(hosts: List[DeployHost]) -> None:
@@ -30,13 +36,7 @@ def deploy_nixos(hosts: List[DeployHost]) -> None:
     g.run_function(deploy)
 
 
-TUM = [
-    "bill.r",
-    "nardole.r",
-    "yasmin.r",
-    "graham.r",
-    "ryan.r"
-]
+TUM = ["bill.r", "nardole.r", "yasmin.r", "graham.r", "ryan.r"]
 EDINBURGH = [
     "rose.r",
     "amy.r",
@@ -64,6 +64,55 @@ def deploy_doctor(c):
     Deploy to rpi4, that is used to control power relay
     """
     deploy_nixos([DeployHost("doctor.r")])
+
+
+def sfdisk_json(host: DeployHost, dev: str) -> List[Any]:
+    out = host.run(f"sfdisk --json {dev}", stdout=subprocess.PIPE)
+    data = json.loads(out.stdout)
+    return data["partitiontable"]["partitions"]
+
+
+def _format_disks(host: DeployHost, device: str) -> None:
+    # format disk with as follow:
+    # - partition 1 will be the boot partition
+    # - partition 2 takes up the rest of the space and is for the system
+    host.run(f"blkdiscard -f {device}")
+    host.run(f"sgdisk -Z -n 1:2048:+1G -N 2 -t 1:ef02 -t 2:8304 {device}")
+
+    partitions = sfdisk_json(host, device)
+    boot = partitions[0]["node"]
+    uuid = partitions[1]["uuid"].lower()
+    root_part = f"/dev/disk/by-partuuid/{uuid}"
+    host.run(
+        f"zpool create zroot -O acltype=posixacl -O xattr=sa -O compression=lz4 -O atime=off {root_part}"
+    )
+
+    host.run(f"partprobe")
+    host.run(f"mkfs.vfat {boot} -n NIXOS_BOOT")
+
+    # setup zfs dataset
+    host.run(f"zfs create -o mountpoint=none zroot/root")
+    host.run(f"zfs create -o mountpoint=legacy zroot/root/nixos")
+    host.run(f"zfs create -o mountpoint=legacy zroot/root/home")
+
+    ## and finally mount
+    host.run(f"mount -t zfs zroot/root/nixos /mnt")
+    host.run(f"mkdir /mnt/home /mnt/boot")
+    host.run(f"mount -t zfs zroot/root/home /mnt/home")
+    host.run(f"mount /dev/disk/by-label/NIXOS_BOOT /mnt/boot")
+
+
+@task
+def format_disks(c, hosts="", disk=""):
+    """
+    Format disks with zfs, i.e.: inv format-disks --hosts new-hostname --disk /dev/nvme0n1
+    """
+    if hosts == "":
+        print("no `--hosts` provided")
+        return
+
+    for h in get_hosts(hosts):
+        _format_disks(h, disk)
 
 
 @task
