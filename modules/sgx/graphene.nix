405cc365b7c44b4b2d79d1202c4b784bcbcc3cf7
Jörg Thalheim <joerg@thalheim.io>
2021-12-23 14:23:34 +0100

    update kernel for graphene

--- modules/sgx/graphene.nix
+++ modules/sgx/graphene.nix
@@ -9,5 +9,5 @@ in
   boot.extraModulePackages = [ graphene-sgx-driver ];
 
   # we need linux 5.9+: https://github.com/oscarlab/graphene-sgx-driver/issues/31
-  boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_5_14;
+  boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_5_15;
 }
