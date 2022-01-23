beffc008384f37bed2b3c9eb6bf448da719228eb
Jörg Thalheim <joerg@thalheim.io>
2022-01-23 12:49:16 +0100

    swiss-knife: also convert keys to list

--- modules/users.nix
+++ modules/users.nix
@@ -37,7 +37,6 @@ let
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINOtq2p1WbvKsV63p1+GhgfDxMmN3dFUL+IM1/HcWSqJ dimstav23@gmail.com"
   ];
 
-  # Lorenzo
   s1443541Keys = [
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDMGYuFUPoNlV6I5H5dxqiGjp39w+8Yrj96NjEicr7Fgl4OTIvCw2++MscyLsVsLnT0CQv9AKMiKNzRPJ/GYOMwgMIDZ9Sfa0T6jPjHghjCHB7UPt7X6NZCEyqoh2R1TpfkJ4hewvH8DWGQcvtDLd37URFIQzFet8Km6unLJPbWbcdoAd1HQZd8Ww5a4YQR2Ek+ZjIp1cm4w/YtZaiLqsSgDFedglZVGCvhG0xQs+esN2HdvvlhkcArDw74/LeJo9HkvK+QJSMqrXHd+1BHk/gtxmE2A5t7uYsyfH73QZffh0eVtiZbDG9hwEE9mmnFD83do7delEdAUGJR/n1EJ+6z"
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC8d4N+83o7l4OHu9awJkrPN27Rbhzx7WmmhmRs8q9UpAuoj9HgoA57JWlIz/EyyQYxjkK7su7emX5Thprg8AwKvI90rFPM2xO7GSuzAWx44++JhlQ7VgHAy9NIiBhLjg04AuK8pNKEWDu8D/sY7GGVMbcDJnOC+pxbX37qZb/I6liAKYfs8BwWasCgXf3JwdM7pj5ijYbfdxy3H2qU5STJkLEBQZF7vTOCLrKvbVswYKFh3DZJbKvqGVIqnmhhvkOUEr5No9YQtgJm5hM7ZJEnCtNeQbKyzIhcW2qOK5yilJrqHcVFI+o6dqL/V1QZB3um6EpEoOfY1483hR9HD+/remCHHxFwiDpptzSuPdW5Hpoe3J4aSHI7znJKI8IW/ugK7o+Xy4Wau4SdUu40kJsnHWEiOUL9M9ZJ7b7XTrAT9qv2KmMl17Sw1qZSYp5SFdVkfUnTCdZF1Zf8leT6WW0LTIuyUESfxfDB3OKlBmAJFNg4L42SjtJLrwuXCgugnhKVp/xNbA+0YHS0Lgy8TJbXkgd/xVekFdhwC7w9Iqszi4KTmGQilPLA2XJfndUedPphDD1Xq1dFWRG9fnKImzmW7JpCvAUHFUrdAqCcJ0Eg4RNkfK4aYFGYk54d/yI6beJPvshjhNf1UqWJqbUdy+jn0E5N53BNnTWFj3Rk/O2uXw=="
@@ -54,7 +53,6 @@ let
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDS5MBL3BDvTOlO0baxN9sXeo0fjhCA8U71sSMLwWCNx6Y/L+aMXRQrimnu7K1x7oM/BuV7IzAosV2lZe7mnD2Lvs9kzWe8KwNR9m9fUV54PTqR6Yjg+f13JB1/KGWd1SmyCOGXXZCG5K3HJqK5Rju4VhlJUEGRQ3dl2bV1l9E8hyHNL0CQWKbIMDbHv19vMtAqEfIHCDqFkf7+gO7Fx5/EJ+2Tt3s6xTx4tse+0k6R2KcwOB/ArlUEN8ye4jO4/sNcyAzY7z8OukuDB4ky2TxJp2C0ljWpkUIcAk4eOS8MXKMy5OSfA7ev+PdpI2lYw3VhH112bZZ3XqW16YNCj6Xf iriditsa@trypokarydos"
   ];
 
-  # Myoung Jin Nam
   mjnamKeys = [
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC9B7L8nxes3S4NekAKKGYl0DTrtWcak/Cznk60FxrQeEeMW3X3O/TNx++keMBfTpi+crzVbFvpJQxAsG3CsWTMK3OrW9HJJvDajQqgd3YJt43cn++EVBk6JQK/J08mvKz5/dFx8W7mfg5IljP1LMjbTJLEOy/slJjXIfuVt4Fvb5v2uqVvbzqRyIIACCJhlKeM1mnYsT0OItPq8cinUZrb5haxCuSRywgpjwnxuOZff4m8x5EkYJ1X2kKgAm8VbVpTHqQJ2d0tBmNkHw52m7hD1dvAUDbTC2Fum10cyrCX1Fw2y+a8br2Wc1wipkzZI3Vm83LKSNaBxuRQ3YXf8+J8RjmXY85f920IblVBEkVZnG5xDadVrX/neBX8e/tYL/V86LKvZMvEYV0Yq0IR4op04kyFhdpGD2IEMu4aAKH2wtAgrpaz+3T/xrS9Sg1ZKSEqOSX1kOvbVXLUCxPFqqk8eibQGsIo3pyplOvCNJVTKkO25ss4W77xq10YCUu6T90= poolmoo@Jin-X13-linux"
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCavNkgsOAT2DzG/KEphfE0Mi3ymBm5ChvWiN5CPYiTs4Ygsf1eBN1KS4x57pPHG04XYOwRnhYq3LgWsbo/Cg8q1plBHGN1DJhLjhrxCQhan3zqhj7iOaDLPawEiupuUQ6D4cMzdlTHEK7iGyfq7pe84KBfW2l/PyQ6Dt92bxPgOut3x7/iVhhm03AQ2JQYwSp/ALrKnrHeT0yxk+vKkvKlMKhDf847XTRiwDHbq/d2oD0Tx6DntwfMte0Yi1UvCgAHos7nbgiXnr1zYdqrg5/daFXFr9tuPCEOEqhkHTe4EIgfnAXycBnFhGSlJFtkPOpvRIzF3YpIoyJ6vpS0t6w9f15zzXtk2Hy+ZmwhlhFI+dXl5QQ10samtD1ctuxSSWSbrc+50NCf1QfNxk+MQnGzEZT7JYZk3kra/hO/VNNBa8FA65vkZyaxzbhYM+a+81rXSRN20vMyF7h0vkHQUCMzqIQWRD1PAh+5AvBgUoruijkKrWIiNgCgP/TGMzUObD8= poolmoo@Jin-ThinkPad-T15g-Gen-1"
@@ -66,7 +64,6 @@ let
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDITBcN9iw5Fn7yyfgiWFet3QWDoMcUNtzLi+PNoYS7jksvcKZy5pLOjE6wCpkbYx+Tcb4MyvoWPXvwdo5FfL4XdhZRO+JlZ66p/rGssq/wEr2BBUwohP7o39JLtiyXGXSsK6MO2aceOFLQr4KAdaeD8ST0XumGcV6bGqIbjFsK5FCxFhO8NkCFtavBjDwKUm3uyOnVCWMp12abUphzxrVtWhcsnw5GapohATP03mCNxmrn/L7x393HutxgjyduScX7++MjwVE6J7wCnztPUtJbh9jYemr/K9fBMBbLhQagOjrlQYGU5frgmLrPCRZusyg5HjWx6gJIxs/DskfgmW+V"
   ];
   
-  # Simon Ellmann
   ackxolotlKeys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIa2YRaVvN0qZ1OD1x1zHQMW7RZ5dBCCenHbb/AUV46H ackxolotl"
   ];
@@ -81,6 +78,7 @@ in
   ];
 
   users.users = {
+    # Jörg Thalheim
     # tum/edinburgh
     joerg = {
       isNormalUser = true;
@@ -91,6 +89,7 @@ in
       openssh.authorizedKeys.keys = joergsKeys;
     };
 
+    # Dimitra Giantsidi
     # edinburgh
     dimitra = {
       isNormalUser = true;
@@ -101,6 +100,7 @@ in
       openssh.authorizedKeys.keys = dimitraKeys;
     };
 
+    # Maurice Bailleu 
     # edinburgh
     maurice = {
       isNormalUser = true;
@@ -111,6 +111,7 @@ in
       openssh.authorizedKeys.keys = mauriceKeys;
     };
 
+    # Lorenzo
     # edinburgh
     s1443541 = {
       isNormalUser = true;
@@ -121,7 +122,8 @@ in
       openssh.authorizedKeys.keys = s1443541Keys;
     };
 
-    # edinburgh
+    # Dimitris Stavrakakis
+    # tum
     dimitrios = {
       isNormalUser = true;
       home = "/home/dimitrios";
@@ -131,6 +133,7 @@ in
       openssh.authorizedKeys.keys = dimitriosKeys;
     };
 
+    # Harshavardhan Unnibhavi
     # tum
     harshanavkis = {
       isNormalUser = true;
@@ -162,6 +165,7 @@ in
       openssh.authorizedKeys.keys = sandroKeys;
     };
 
+    # Redha Gouicem
     # tum
     redha = {
       isNormalUser = true;
@@ -172,6 +176,7 @@ in
       openssh.authorizedKeys.keys = redhaKeys;
     };
 
+    # Atsushi Koshiba
     # tum
     atsushi = {
       isNormalUser = true;
@@ -182,6 +187,7 @@ in
       openssh.authorizedKeys.keys = atsushiKeys;
     };
 
+    # Myoung Jin Nam
     # tum
     mjnam = {
       isNormalUser = true;
@@ -192,6 +198,8 @@ in
       openssh.authorizedKeys.keys = mjnamKeys;
     };
 
+    # Peter Okelmann
+    # tum
     okelmann = {
       isNormalUser = true;
       home = "/home/okelmann";
@@ -201,6 +209,8 @@ in
       openssh.authorizedKeys.keys = okelmannKeys;
     };
     
+    # Simon Ellmann
+    # tum
     ackxolotl = {
       isNormalUser = true;
       home = "/home/ackxolotl";
