7f713679b9674705ad6c7befba61581f7d6429a9
Jörg Thalheim <joerg@thalheim.io>
2022-02-04 08:42:43 +0100

    rose: add picocom for serial debugging

--- hosts/rose.nix
+++ hosts/rose.nix
@@ -9,6 +9,12 @@
     ipv6 = "42:0:3c46:a5a1:618d:663a:f0f1:ec88";
   };
 
+  environment.systemPackages = [
+    # on /dev/ttyUSB0 doctor is connected via serial
+    # $ sudo picocom -b 115200 /dev/ttyUSB0
+    pkgs.picocom
+  ];
+
   boot.hugepages.number = 8000;
 
   system.stateVersion = "19.09";
