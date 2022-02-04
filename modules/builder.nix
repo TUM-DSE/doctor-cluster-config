9fc4f5b69a2b62ec11dca709c4380a01ded86202
Jörg Thalheim <joerg@thalheim.io>
2022-02-04 23:25:12 +0100

    builder: drop hydra key

--- modules/builder.nix
+++ modules/builder.nix
@@ -4,7 +4,6 @@
     home = "/home/nix";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF48CsASF4l2oVA9GNNi0LCd4ONOtf0zkQx1tUbhSW3S joerg@turingmachine"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPS4sLGbDPZ4UDT3Rhy8uAz5vduRUAr0uEvdnUBP0cm4 hydra@eve"
     ];
     uid = 2001;
   };
