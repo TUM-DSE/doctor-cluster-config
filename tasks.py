796e0d69acb88bec7150212a4edef5b438a878d9
Jörg Thalheim <joerg@thalheim.io>
2022-03-28 07:47:05 +0200

    ipmi-serial: enable pty

--- tasks.py
+++ tasks.py
@@ -255,7 +255,8 @@ def ipmi_password(c) -> str:
 @task
 def ipmi_serial(c, host=""):
     c.run(
-        f"""ipmitool -I lanplus -H {host} -U ADMIN -P '{ipmi_password(c)}' sol activate"""
+        f"""ipmitool -I lanplus -H {host} -U ADMIN -P '{ipmi_password(c)}' sol activate""",
+        pty=True
     )
 
 
