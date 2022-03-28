d5ee7298c95c419449c802977aa5e93116a84887
Jörg Thalheim <joerg@thalheim.io>
2022-03-28 18:14:58 +0200

    add christina

---
+++ hosts/christina.nix
@@ -0,0 +1,14 @@
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  imports = [
+    ../modules/hardware/supermicro-x12spw-tf.nix
+    ../modules/nfs/client.nix
+  ];
+
+  networking.hostName = "christina";
+
+  system.stateVersion = "21.11";
+}
