ba158f7713c56d95bcb599dcd99a29e62ef99374
Pogobanane <aenderboy@gmx.de>
2022-05-02 13:34:45 +0200

    dpdk: remove udev nvme rules

--- modules/dpdk.nix
+++ modules/dpdk.nix
@@ -35,18 +35,5 @@ with lib; {
     boot.extraModprobeConfig = ''
       blacklist ice
     '';
-
-    # From the output of udevadm info -a -p /sys/bus/pci/devices/0000:03:00.0,
-    # where 0000:03:00.0 is our NVME device
-    # Deactivating, because those rules would match any device of the
-    # respective type, regardless of the server this is done on. 
-    #services.udev.extraRules = ''
-    #  # Intel Corporation Express Flash NVMe P4500/P4600
-    #  ATTR{vendor}=="0x8086" ATTR{device}=="0x0a54", RUN+="${pkgs.coreutils}/bin/chmod 666 /sys/bus/pci/devices/$kernel/resource0"
-
-    #  # Intel Corporation Ethernet Controller XL710 for 40GbE QSFP+
-    #  ATTR{vendor}=="0x8086" ATTR{device}=="0x1583", RUN+="${pkgs.coreutils}/bin/chmod 666 /sys/bus/pci/devices/$kernel/resource0"
-    #  ATTR{vendor}=="0x8086" ATTR{device}=="0x1583", RUN+="${pkgs.coreutils}/bin/chmod 666 /sys/bus/pci/devices/$kernel/resource3"
-    #'';
   };
 }
