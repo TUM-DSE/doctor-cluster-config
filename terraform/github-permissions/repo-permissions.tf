27a257e556fd9df3df1b6bbe48e05697f187d062
Pogobanane <aenderboy@gmx.de>
2022-05-03 00:50:52 +0200

    terraform: object based repo permissions

--- terraform/github-permissions/repo-permissions.tf
+++ terraform/github-permissions/repo-permissions.tf
@@ -6,37 +6,45 @@ locals {
     "Mic92"
   ])
 
+  perms-TUM-DSE = {
+    "doctor-cluster-config" = setunion(
+      local.sys-prog-tutors, 
+      local.sys-prog-students
+    )
+    "foo" = toset(["bar"])
+  }
+
   # Nested loop over both lists, and flatten the result.
-  schema_privileges = distinct(flatten([
-    for schema in local. : [
-      for privilege in local.privileges : {
-        privilege = privilege
-        schema    = schema
+  perms1 = distinct(flatten([
+    for repo, users in local.perms-TUM-DSE : [
+      for user in users : {
+        repo = repo
+        user = user
       }
     ]
   ]))
 }
 
 resource "github_repository_collaborator" "doctor-cluster-students" {
-  for_each   = local.sys-prog-tutors
+  for_each   = { for repo in local.perms1 : "${repo.repo}.${repo.user}" => repo }
   provider   = github.TUM-DSE
-  repository = "doctor-cluster-config"
-  username   = each.value
+  repository = each.value.repo
+  username   = each.value.user
   permission = "pull"
 }
 
-resource "github_repository_collaborator" "doctor-cluster-tutors" {
-  for_each   = local.sys-prog-students
-  provider   = github.TUM-DSE
-  repository = "doctor-cluster-config"
-  username   = each.value
-  permission = "pull"
-}
-
-resource "github_repository_collaborator" "add_users2" {
-  for_each   = local.sys-prog-tutors
-  provider   = github.ls1-courses
-  repository = "anti-cheat"
-  username   = each.value
-  permission = "pull"
-}
+#resource "github_repository_collaborator" "doctor-cluster-tutors" {
+#  for_each   = local.sys-prog-students
+#  provider   = github.TUM-DSE
+#  repository = "doctor-cluster-config"
+#  username   = each.value
+#  permission = "pull"
+#}
+#
+#resource "github_repository_collaborator" "add_users2" {
+#  for_each   = local.sys-prog-tutors
+#  provider   = github.ls1-courses
+#  repository = "anti-cheat"
+#  username   = each.value
+#  permission = "pull"
+#}
