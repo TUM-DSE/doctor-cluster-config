b45d7ef4f504612584a1501d1fa47785f9e8654d
Jörg Thalheim <joerg@thalheim.io>
2022-05-04 19:38:41 +0200

    netboot-pixie-core: make it run without sudo

--- modules/netboot/netboot-pixie-core.nix
+++ modules/netboot/netboot-pixie-core.nix
@@ -6,12 +6,13 @@
 # github.com/Mic92/dotfiles/nixos/modules/dnsmasq.nix and follow its
 # instructions.
 # 2. Run the netboot server
-# $ sudo nix run '.#netboot-pixie-core'
+# $ nix run '.#netboot-pixie-core'
 pkgs.writeShellScriptBin "netboot-pixie-core" ''
   set -euo pipefail -x
   cmdline=$(${pkgs.gnugrep}/bin/grep -ohP 'init=.+' ${netboot}/netboot.ipxe)
   cmdline=$(echo "''${cmdline//initrd=initrd /}" )
-  ${pkgs.pixiecore}/bin/pixiecore \
+  # needs root for RAW_SOCKETS
+  sudo ${pkgs.pixiecore}/bin/pixiecore \
     boot ${netboot}/bzImage ${netboot}/initrd \
     --cmdline "$cmdline" \
     --debug --dhcp-no-bind --port 64172 --status-port 64172
