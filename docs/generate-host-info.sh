1882bf3ee8dbe81889cc9fd978aeaee8e9f6f061
Jörg Thalheim <joerg@thalheim.io>
2022-05-09 15:36:17 +0200

    docs/generate-host-info.sh: fixes for macos

--- docs/generate-host-info.sh
+++ docs/generate-host-info.sh
@@ -5,10 +5,17 @@ report="$hostname.md"
 echo -e "# ${hostname}\n" > "$report"
 echo "\`\`\`" >> "$report"
 if [[ "$hostname" == hinoki ]]; then
+  # this is an ubuntu host
   ssh "$1" -- sudo inxi -F -i --slots -xxx -c0 -Z -i -m >> "$report"
+elif [[ "$hostname" == sarah ]]; then
+  # macos does not have dmidecode
+  ssh "$1" -- nix-shell -p inxi --run \"sudo inxi -F -i --slots -xxx -c0 -Z -i -m\" >> "$report"
+  ssh "$1" -- nix-shell -p hwloc --run \"sudo lstopo /tmp/$hostname.lstopo.svg\"
+  scp "$1":/tmp/$hostname.lstopo.svg ./$hostname.lstopo.svg
+  ssh "$1" -- sudo rm /tmp/$hostname.lstopo.svg
 else
-  ssh "$1" -- nix-shell -p inxi -p dmidecode --command \"sudo inxi -F -i --slots -xxx -c0 -Z -i -m\" >> "$report"
-  ssh "$1" -- nix-shell -p hwloc -p dmidecode --command \"sudo lstopo /tmp/$hostname.lstopo.svg\"
+  ssh "$1" -- nix-shell -p inxi -p dmidecode --run \"sudo inxi -F -i --slots -xxx -c0 -Z -i -m\" >> "$report"
+  ssh "$1" -- nix-shell -p hwloc -p dmidecode --run \"sudo lstopo /tmp/$hostname.lstopo.svg\"
   scp "$1":/tmp/$hostname.lstopo.svg ./$hostname.lstopo.svg
   ssh "$1" -- sudo rm /tmp/$hostname.lstopo.svg
 fi
