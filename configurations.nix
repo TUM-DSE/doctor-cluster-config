375f35ce2f498ded317bedd3357f88882fc8b203
Jörg Thalheim <joerg@thalheim.io>
2022-05-23 14:42:36 +0200

    use flake-modules-core

--- configurations.nix
+++ configurations.nix
@@ -1,18 +1,19 @@
-{
-  nixpkgs,
-  nur,
-  home-manager,
-  retiolum,
-  sops-nix,
-  flake-registry,
-  nix-ld,
-  nixos-hardware,
-  inputs,
-  ...
-}: let
+{self, ...}: let
+  inherit
+    (self.inputs)
+    nixpkgs
+    retiolum
+    sops-nix
+    home-manager
+    nur
+    flake-registry
+    nixos-hardware
+    nix-ld
+    ;
   nixosSystem = nixpkgs.lib.makeOverridable nixpkgs.lib.nixosSystem;
+
   commonModules = [
-    {_module.args.inputs = inputs;}
+    {_module.args.inputs = self.inputs;}
     ./modules/packages.nix
     ./modules/nix-daemon.nix
     ./modules/telegraf.nix
@@ -38,16 +39,17 @@
         "nixpkgs=${nixpkgs}"
         "nur=${nur}"
       ];
+      # TODO: share nixpkgs for each machine to speed up local evaluation.
+      #nixpkgs.pkgs = self.inputs.nixpkgs.legacyPackages.${system};
 
       #sops.withSops = true;
       sops.secrets.root-password-hash.neededForUsers = true;
       sops.defaultSopsFile = let
         sopsFile = ./. + "/hosts/${config.networking.hostName}.yml";
       in
-        if builtins.pathExists sopsFile then
-          sopsFile
-        else
-          null;
+        if builtins.pathExists sopsFile
+        then sopsFile
+        else null;
 
       nix.extraOptions = ''
         flake-registry = ${flake-registry}/flake-registry.json
@@ -83,198 +85,199 @@
       ./hardware-configuration.nix
     ];
 in {
-  amy = nixosSystem {
-    system = "x86_64-linux";
-    modules =
-      sgxNodeModules
-      ++ [
-        ./hosts/amy.nix
-      ];
-  };
-
-  clara = nixosSystem {
-    system = "x86_64-linux";
-    modules =
-      sgxNodeModules
-      ++ [
-        ./hosts/clara.nix
-      ];
-  };
-
-  # we need to increase /boot before we can upgrade
-  doctor = nixosSystem {
-    system = "aarch64-linux";
-    modules =
-      commonModules
-      ++ [
-        ./hosts/doctor.nix
-      ];
-  };
-
-  donna = nixosSystem {
-    system = "x86_64-linux";
-    modules =
-      sgxNodeModules
-      ++ [
-        ./hosts/donna.nix
-      ];
-  };
-
-  martha = nixosSystem {
-    system = "x86_64-linux";
-    modules =
-      sgxNodeModules
-      ++ [
-        ./hosts/martha.nix
-      ];
-  };
-
-  rose = nixosSystem {
-    system = "x86_64-linux";
-    modules =
-      sgxNodeModules
-      ++ [
-        ./hosts/rose.nix
-      ];
-  };
-
-  bill = nixosSystem {
-    system = "x86_64-linux";
-    modules =
-      computeNodeModules
-      ++ [
-        ./hosts/bill.nix
-        ./hardware-configuration.nix
-      ];
-  };
-
-  nardole = nixosSystem {
-    system = "x86_64-linux";
-    modules =
-      computeNodeModules
-      ++ [
-        ./hosts/nardole.nix
-        ./hardware-configuration.nix
-      ];
-  };
-
-  sauron = nixosSystem {
-    system = "x86_64-linux";
-    modules =
-      sgxNodeModules
-      ++ [
-        ./hosts/sauron.nix
-      ];
-  };
-
-  yasmin = nixosSystem {
-    system = "aarch64-linux";
-    modules =
-      computeNodeModules
-      ++ [
-        ./hosts/yasmin.nix
-      ];
-  };
-
-  graham = nixosSystem {
-    system = "x86_64-linux";
-    modules =
-      computeNodeModules
-      ++ [
-        ./hosts/graham.nix
-        nix-ld.nixosModules.nix-ld
-        nixos-hardware.nixosModules.dell-poweredge-r7515
-      ];
-  };
-
-  ryan = nixosSystem {
-    system = "x86_64-linux";
-    modules =
-      computeNodeModules
-      ++ [
-        ./hosts/ryan.nix
-      ];
-  };
-
-  mickey = nixosSystem {
-    system = "x86_64-linux";
-    modules =
-      computeNodeModules
-      ++ [
-        ./hosts/mickey.nix
-      ];
-  };
-
-  astrid = nixosSystem {
-    system = "x86_64-linux";
-    modules =
-      computeNodeModules
-      ++ [
-        ./hosts/astrid.nix
-      ];
-  };
-
-  dan = nixosSystem {
-    system = "x86_64-linux";
-    modules =
-      computeNodeModules
-      ++ [
-        ./hosts/dan.nix
-      ];
-  };
-
-  christina = nixosSystem {
-    system = "x86_64-linux";
-    modules =
-      computeNodeModules
-      ++ [
-        ./hosts/christina.nix
-      ];
-  };
-
-  jackson = nixosSystem {
-    system = "x86_64-linux";
-    modules =
-      computeNodeModules
-      ++ [
-        ./hosts/jackson.nix
-      ];
-  };
-
-  adelaide = nixosSystem {
-    system = "x86_64-linux";
-    modules =
-      computeNodeModules
-      ++ [
-        ./hosts/adelaide.nix
-      ];
+  flake.nixosConfigurations = {
+    amy = nixosSystem {
+      system = "x86_64-linux";
+      modules =
+        sgxNodeModules
+        ++ [
+          ./hosts/amy.nix
+        ];
     };
 
-  wilfred = nixosSystem {
-    system = "x86_64-linux";
-    modules =
-      computeNodeModules
-      ++ [
-        ./hosts/wilfred.nix
-      ];
+    clara = nixosSystem {
+      system = "x86_64-linux";
+      modules =
+        sgxNodeModules
+        ++ [
+          ./hosts/clara.nix
+        ];
     };
 
-  river = nixosSystem {
-    system = "x86_64-linux";
-    modules =
-      computeNodeModules
-      ++ [
-        ./hosts/river.nix
-      ];
-  };
+    # we need to increase /boot before we can upgrade
+    doctor = nixosSystem {
+      system = "aarch64-linux";
+      modules =
+        commonModules
+        ++ [
+          ./hosts/doctor.nix
+        ];
+    };
 
-  jack = nixosSystem {
-    system = "x86_64-linux";
-    modules =
-      computeNodeModules
-      ++ [
-        ./hosts/jack.nix
-      ];
-  };
+    donna = nixosSystem {
+      system = "x86_64-linux";
+      modules =
+        sgxNodeModules
+        ++ [
+          ./hosts/donna.nix
+        ];
+    };
 
+    martha = nixosSystem {
+      system = "x86_64-linux";
+      modules =
+        sgxNodeModules
+        ++ [
+          ./hosts/martha.nix
+        ];
+    };
+
+    rose = nixosSystem {
+      system = "x86_64-linux";
+      modules =
+        sgxNodeModules
+        ++ [
+          ./hosts/rose.nix
+        ];
+    };
+
+    bill = nixosSystem {
+      system = "x86_64-linux";
+      modules =
+        computeNodeModules
+        ++ [
+          ./hosts/bill.nix
+          ./hardware-configuration.nix
+        ];
+    };
+
+    nardole = nixosSystem {
+      system = "x86_64-linux";
+      modules =
+        computeNodeModules
+        ++ [
+          ./hosts/nardole.nix
+          ./hardware-configuration.nix
+        ];
+    };
+
+    sauron = nixosSystem {
+      system = "x86_64-linux";
+      modules =
+        sgxNodeModules
+        ++ [
+          ./hosts/sauron.nix
+        ];
+    };
+
+    yasmin = nixosSystem {
+      system = "aarch64-linux";
+      modules =
+        computeNodeModules
+        ++ [
+          ./hosts/yasmin.nix
+        ];
+    };
+
+    graham = nixosSystem {
+      system = "x86_64-linux";
+      modules =
+        computeNodeModules
+        ++ [
+          ./hosts/graham.nix
+          nix-ld.nixosModules.nix-ld
+          nixos-hardware.nixosModules.dell-poweredge-r7515
+        ];
+    };
+
+    ryan = nixosSystem {
+      system = "x86_64-linux";
+      modules =
+        computeNodeModules
+        ++ [
+          ./hosts/ryan.nix
+        ];
+    };
+
+    mickey = nixosSystem {
+      system = "x86_64-linux";
+      modules =
+        computeNodeModules
+        ++ [
+          ./hosts/mickey.nix
+        ];
+    };
+
+    astrid = nixosSystem {
+      system = "x86_64-linux";
+      modules =
+        computeNodeModules
+        ++ [
+          ./hosts/astrid.nix
+        ];
+    };
+
+    dan = nixosSystem {
+      system = "x86_64-linux";
+      modules =
+        computeNodeModules
+        ++ [
+          ./hosts/dan.nix
+        ];
+    };
+
+    christina = nixosSystem {
+      system = "x86_64-linux";
+      modules =
+        computeNodeModules
+        ++ [
+          ./hosts/christina.nix
+        ];
+    };
+
+    jackson = nixosSystem {
+      system = "x86_64-linux";
+      modules =
+        computeNodeModules
+        ++ [
+          ./hosts/jackson.nix
+        ];
+    };
+
+    adelaide = nixosSystem {
+      system = "x86_64-linux";
+      modules =
+        computeNodeModules
+        ++ [
+          ./hosts/adelaide.nix
+        ];
+    };
+
+    wilfred = nixosSystem {
+      system = "x86_64-linux";
+      modules =
+        computeNodeModules
+        ++ [
+          ./hosts/wilfred.nix
+        ];
+    };
+
+    river = nixosSystem {
+      system = "x86_64-linux";
+      modules =
+        computeNodeModules
+        ++ [
+          ./hosts/river.nix
+        ];
+    };
+
+    jack = nixosSystem {
+      system = "x86_64-linux";
+      modules =
+        computeNodeModules
+        ++ [
+          ./hosts/jack.nix
+        ];
+    };
+  };
 }
