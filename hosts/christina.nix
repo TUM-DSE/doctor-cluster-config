0ca8369bcd80d17aad66081d675c45a3fb3788fe
dimstav23 <dimstav23@gmail.com>
2022-05-30 11:48:15 +0000

    add the dax module in christina

--- hosts/christina.nix
+++ hosts/christina.nix
@@ -6,7 +6,10 @@
   imports = [
     ../modules/hardware/supermicro-x12spw-tf.nix
     ../modules/nfs/client.nix
+    ../modules/dax.nix
   ];
+
+  doctorwho.pmem.devices = [ "pmem0" ];
 
   networking.hostName = "christina";
 
