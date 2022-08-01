a5e9cfe905ce9fc1e1d55abcdd866e03c974f63c
Jörg Thalheim <joerg@thalheim.io>
2022-08-01 11:05:01 +0200

    add zsh-nix-shell plugin

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -1,5 +1,8 @@
-{
+{ pkgs, ...}: {
   programs.zsh.enable = true;
   programs.zsh.enableCompletion = true;
   programs.zsh.enableGlobalCompInit = false;
+  programs.zsh.interactiveShellInit = ''
+    source ${pkgs.zsh-nix-shell}/share/zsh-nix-shell/nix-shell.plugin.zsh
+  '';
 }
