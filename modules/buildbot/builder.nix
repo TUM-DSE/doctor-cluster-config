9572fa180ab24eab881527d165b5c2f2f3158ae5
Jörg Thalheim <joerg@thalheim.io>
2022-08-08 17:01:33 +0200

    add yasmin as worker

---
+++ modules/buildbot/builder.nix
@@ -0,0 +1,11 @@
+{
+  users.extraUsers.buildbot-worker = {
+    isNormalUser = true;
+    home = "/var/lib/buildbot-worker";
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID/yryxd93+2KkqhCaJIjXf48jPre3HnHyvHz/aQPlHt"
+    ];
+    uid = 5003;
+  };
+  nix.trustedUsers = ["buildbot-worker"];
+}
