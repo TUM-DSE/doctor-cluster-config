2edf708dd69353867eef85aeba521fe7575d980b
Atsushi Koshiba <cos24boc@gmail.com>
2022-11-02 14:37:30 +0100

    fpga: disable XRT drivers

--- modules/xilinx.nix
+++ modules/xilinx.nix
@@ -19,9 +19,10 @@ in {
   ];
 
   boot.kernelPackages = pkgs.linuxPackages_5_10;
-  boot.extraModulePackages = [
-    xrt-drivers
-  ];
+  # The kernel drivers are not required this time. 
+  # boot.extraModulePackages = [
+  #   xrt-drivers
+  # ];
 
   hardware.opengl.extraPackages = [
     packages.xrt
