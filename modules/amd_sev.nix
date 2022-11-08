541bfbb54189d48e1f6815ecc1d76aa0508635ce
dimstav23 <dimstav23@gmail.com>
2022-11-08 22:22:59 +0000

    enable amd sme

--- modules/amd_sev.nix
+++ modules/amd_sev.nix
@@ -6,6 +6,20 @@
 }: {
   # This is used to enable AMD-SEV
   config = {
+    # enable AMD-SME
+    # https://cateee.net/lkddb/web-lkddb/AMD_MEM_ENCRYPT.html
+    boot.kernelPatches = [
+      {
+        name = "amd_sme-config";
+        patch = null;
+        extraConfig = ''
+          AMD_MEM_ENCRYPT y
+        '';
+      }
+    ];
+
+    # enable AMD-SEV
+    # https://libvirt.org/kbase/launch_security_sev.html#enabling-sev-on-the-host
     boot.kernelParams = [
       "mem_encrypt=on"
       "kvm_amd.sev=1"
