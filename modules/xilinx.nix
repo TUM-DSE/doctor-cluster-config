f9af029708422f33b68a0e8df669ed417b6a5094
Jörg Thalheim <joerg@thalheim.io>
2022-11-15 09:32:51 +0100

    xilinx: if xrt is not enabled don't downgrade kernel

--- modules/xilinx.nix
+++ modules/xilinx.nix
@@ -2,59 +2,64 @@
   pkgs,
   config,
   self,
+  lib,
   ...
 }: let
   packages = self.packages.${pkgs.system};
   xrt-drivers = packages.xrt-drivers.override { inherit (config.boot.kernelPackages) kernel; };
 in {
-  environment.systemPackages = [
-    (packages.xilinx-env.override {
-      xilinxName = "xilinx-shell";
-      runScript = "bash";
-    })
-    (packages.xilinx-env.override {
-      xilinxName = "vitis";
-      runScript = "vitis";
-    })
-  ];
 
-  services.udev.packages = [ packages.xilinx-cable-drivers ];
-
-  boot.kernelPackages = pkgs.linuxPackages_5_10;
-  ### Comment out here to unload XRT drivers 
-  ### if you want to test custom hardware designs other than XRT apps (e.g., Coyote). 
-  # boot.extraModulePackages = [
-  #   xrt-drivers
-  # ];
-
-  hardware.opengl.extraPackages = [
-    packages.xrt
-  ];
-
-  systemd.tmpfiles.rules = [
-    "L+ /opt/xilinx/xrt - - - - ${packages.xrt}/opt/xilinx/xrt"
-  ];
-
-  systemd.services.setup-xilinx-firmware = {
-    wantedBy = ["multi-user.target"];
-    script = ''
-      rm -rf /lib/firmware/xilinx /opt/xilinx/firmware
-      mkdir -p /lib/firmware/xilinx /opt/xilinx/firmware
-      cp -r ${packages.xilinx-firmware}/lib/firmware/xilinx/* /lib/firmware/xilinx/
-      cp -r ${packages.xilinx-firmware}/opt/xilinx/firmware/* /opt/xilinx/firmware/
-      for p in ${packages.xilinx-firmware}/share/xilinx-firmware/*; do
-         echo $p
-         $p
-      done
-    '';
+  options = {
+    hardware.xilinx.xrt-drivers.enable = lib.mkEnableOption "Propritary kernel drivers for flashing firmware";
   };
 
-  sops.secrets.xilinx-password-hash.neededForUsers = true;
+  config = {
+    environment.systemPackages = [
+      (packages.xilinx-env.override {
+        xilinxName = "xilinx-shell";
+        runScript = "bash";
+      })
+      (packages.xilinx-env.override {
+        xilinxName = "vitis";
+        runScript = "vitis";
+      })
+    ];
 
-  users.extraUsers.xilinx = {
-    isNormalUser = true;
-    passwordFile = config.sops.secrets.xilinx-password-hash.path;
-    extraGroups = ["wheel" "docker" "plugdev" "input"];
-    uid = 5002;
+    services.udev.packages = [ packages.xilinx-cable-drivers ];
+
+    boot.kernelPackages =
+      lib.mkIf (config.hardware.xilinx.xrt-drivers.enable) pkgs.linuxPackages_5_10;
+    boot.extraModulePackages = lib.optional (config.hardware.xilinx.xrt-drivers.enable) xrt-drivers;
+
+    hardware.opengl.extraPackages = [
+      packages.xrt
+    ];
+
+    systemd.tmpfiles.rules = [
+      "L+ /opt/xilinx/xrt - - - - ${packages.xrt}/opt/xilinx/xrt"
+    ];
+
+    systemd.services.setup-xilinx-firmware = {
+      wantedBy = ["multi-user.target"];
+      script = ''
+        rm -rf /lib/firmware/xilinx /opt/xilinx/firmware
+        mkdir -p /lib/firmware/xilinx /opt/xilinx/firmware
+        cp -r ${packages.xilinx-firmware}/lib/firmware/xilinx/* /lib/firmware/xilinx/
+        cp -r ${packages.xilinx-firmware}/opt/xilinx/firmware/* /opt/xilinx/firmware/
+        for p in ${packages.xilinx-firmware}/share/xilinx-firmware/*; do
+           echo $p
+           $p
+        done
+      '';
+    };
+
+    sops.secrets.xilinx-password-hash.neededForUsers = true;
+
+    users.extraUsers.xilinx = {
+      isNormalUser = true;
+      passwordFile = config.sops.secrets.xilinx-password-hash.path;
+      extraGroups = ["wheel" "docker" "plugdev" "input"];
+      uid = 5002;
+    };
   };
 }
