69fa00204a02dced8ce9a301ec885c13bfdeef43
Jörg Thalheim <joerg@thalheim.io>
2022-11-21 09:08:00 +0100

    ruby: enable lm_sensors

--- hosts/ruby.nix
+++ hosts/ruby.nix
@@ -18,7 +18,21 @@
 
   environment.systemPackages = with pkgs; [
     vim
+    lm_sensors
   ];
+
+  #hardware.fancontrol.enable = true;
+  #hardware.fancontrol.config = ''
+  #  # Helios4 PWM Fan Control Configuration
+  #  # Temp source : /dev/thermal-cpu
+  #  INTERVAL=10
+  #  FCTEMPS/dev/fan-j10/pwm1=/dev/thermal-cpu/temp1_input /dev/fan-j17/pwm1=/dev/thermal-cpu/temp1_input
+  #  MINTEMP=/dev/fan-j10/pwm1=40 /dev/fan-j17/pwm1=40
+  #  MAXTEMP=/dev/fan-j10/pwm1=80 /dev/fan-j17/pwm1=80
+  #  MINSTART=/dev/fan-j10/pwm1=20 /dev/fan-j17/pwm1=20
+  #  MINSTOP=/dev/fan-j10/pwm1=29 /dev/fan-j17/pwm1=29
+  #  MINPWM=0
+  #'';
 
   services.openssh.enable = true;
 
