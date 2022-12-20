54bf366bbf4356bbdb9a08f0e15134e572f83a45
Sandro-Alessio Gierens <sandro@gierens.de>
2022-12-20 19:45:51 +0100

    Update package to 5.15 in linux ioregionfd module

--- modules/linux-ioregionfd.nix
+++ modules/linux-ioregionfd.nix
@@ -1,5 +1,5 @@
 { pkgs, lib, ...}: let 
-  linux_ioregionfd = pkgs.callPackage ../pkgs/kernels/linux-ioregionfd-5.14.nix { };
+  linux_ioregionfd = pkgs.callPackage ../pkgs/kernels/linux-ioregionfd-5.15.nix { };
   linuxPackages_ioregionfd = pkgs.recurseIntoAttrs 
     (pkgs.linuxPackagesFor linux_ioregionfd);
 in {
