9244836e58378544e645c2caa0aa86791e176fa0
Atsushi Koshiba <cos24boc@gmail.com>
2023-02-15 16:30:01 +0100

    configure xilinx setups for clara, amy, rose

--- hosts/clara.nix
+++ hosts/clara.nix
@@ -3,10 +3,26 @@
     ../modules/ipmi-supermicro.nix
     ../modules/hardware/supermicro-AS-4124GS.nix
     ../modules/nfs/client.nix
+
+    ../modules/xilinx.nix
+    ../modules/xrdp.nix
+    ../modules/xrdp-passwords.nix
   ];
 
   networking.hostName = "clara";
 
   simd.arch = "znver3";
   system.stateVersion = "22.11";
+
+  # manually added to load xilinx from
+  fileSystems."/share" = {
+    device = "nfs:/export/share";
+    fsType = "nfs4";
+    options = [
+      "nofail"
+      "ro"
+      "timeo=14"
+    ];
+  };
+  users.xrdpUsers = [ "atsushi" ];
 }
