c759d413024092bc361c938937212840dee2ffe1
Jörg Thalheim <joerg@thalheim.io>
2023-03-21 20:04:58 +0100

    docker: disable zfs on riscv

--- modules/docker.nix
+++ modules/docker.nix
@@ -1,4 +1,4 @@
-{
+{ lib, pkgs, ... }: {
   # For docker
   boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
   boot.kernel.sysctl."net.ipv6.conf.all.forwarding" = 1;
@@ -6,8 +6,8 @@
   virtualisation.docker = {
     enable = true;
     enableOnBoot = true;
-    storageDriver = "zfs";
-    extraOptions = "--userland-proxy=false --ip-masq=true --storage-opt=zfs.fsname=zroot/docker";
+    storageDriver = if pkgs.hostPlatform.isRiscV then "overlay2" else "zfs";
+    extraOptions = "--userland-proxy=false --ip-masq=true ${lib.optionalString (!pkgs.hostPlatform.isRiscV) "--storage-opt=zfs.fsname=zroot/docker"}";
 
     # not compatible with docker swarm
     liveRestore = false;
