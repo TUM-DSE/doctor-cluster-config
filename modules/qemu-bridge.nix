e297136dad1c23222840a4e76562acd0491c5108
Pogobanane <aenderboy@gmx.de>
2023-04-06 19:53:41 +0200

    qemu-bridge: global qemu-bridge-helper

--- modules/qemu-bridge.nix
+++ modules/qemu-bridge.nix
@@ -1,4 +1,4 @@
-{
+{ pkgs, ... }: {
   # Allow qemu to access bridges
   environment.etc."qemu/bridge.conf" = {
     user = "root";
@@ -6,6 +6,16 @@
     mode = "640";
     text = "allow all";
   };
+
+  # bridge.conf location is hardcoded in qemu-bridge-helper. Thus we expose a correct qemu-bridge-helper globally
+  environment.systemPackages = [
+    (pkgs.writeShellApplication {
+      name = "qemu-bridge-helper";
+      text = ''
+        ${pkgs.qemu}/libexec/qemu-bridge-helper "$@"
+      '';
+    })
+  ];
 
   # Don't manage tap devices with systemd-networkd
   systemd.network.networks."06-tap".extraConfig = ''
