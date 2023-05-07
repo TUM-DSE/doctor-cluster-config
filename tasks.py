c3694097f5ad132bb90709be158f10fbeb75b1d2
Jörg Thalheim <joerg@thalheim.io>
2023-05-07 09:24:51 +0200

    tasks: reformat

--- tasks.py
+++ tasks.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
 
-from invoke import task
-
+import json
+import os
+import random
+import shutil
+import string
 import subprocess
 import sys
-import os
-import json
-import random
-import string
-import shutil
 import tempfile
 from pathlib import Path
-from typing import List, Any
-from deploykit import DeployHost, DeployGroup
-from typing import IO, Any, Callable, List, Dict, Optional, Text
 from string import Template
+from typing import IO, Any, Callable, List
+
+from deploykit import DeployGroup, DeployHost
+from invoke import task
 
 ROOT = Path(__file__).parent.resolve()
 os.chdir(ROOT)
@@ -97,7 +96,7 @@ def document_cards(hosts: DeployGroup) -> str:
         dmi_slots = h.run(
             "nix-shell -p 'dmidecode' --run \"sudo dmidecode -t slot\"",
             stdout=subprocess.PIPE,
-            check=False
+            check=False,
         )
         # fails on our m1 aarch64 linux machine
         if dmi_slots.returncode != 0:
@@ -260,7 +259,7 @@ info = color_text(32)
 
 
 @task
-def deploy(c):
+def deploy(c: Any) -> None:
     """
     Deploy to servers
     """
@@ -268,7 +267,7 @@ def deploy(c):
 
 
 @task
-def deploy_ruby(c):
+def deploy_ruby(c: Any) -> None:
     """
     Deploy to riscv server
     """
@@ -285,6 +284,7 @@ def deploy_ruby(c):
         ),
     )
     deploy_nixos([host])
+
 
 @task
 def deploy_doctor(c):
@@ -320,7 +320,7 @@ def deploy_local(c):
     Deploy NixOS configuration on the same machine. The NixOS configuration is
     selected based on the hostname.
     """
-    c.run(f"""sudo nixos-rebuild switch --flake .#""")
+    c.run("""sudo nixos-rebuild switch --flake .#""")
 
 
 @task
@@ -334,6 +334,7 @@ def update_docs(c, hosts=""):
         host_list = HOSTS
     document_nixos(host_list)
 
+
 @task
 def document_craig(c):
     """
@@ -342,7 +343,9 @@ def document_craig(c):
     # needs encryption because i dont trust the "encryption" used by the admin password found in that file
     craig_sops = f"{ROOT}/docs/hosts/craig.sops"
     with tempfile.TemporaryDirectory() as tmpdir:
-        c.run(f"ssh ADMIN@craig-mgmt.dse.in.tum.de 'no cli pagination; show startup-config; exit' > {tmpdir}/craig.txt || true") # ssh always terminates with 255
+        c.run(
+            f"ssh ADMIN@craig-mgmt.dse.in.tum.de 'no cli pagination; show startup-config; exit' > {tmpdir}/craig.txt || true"
+        )  # ssh always terminates with 255
         print("Diff old <> new config:")
         c.run(f"diff <(sops -d {craig_sops}) {tmpdir}/craig.txt || true")
         print("Diff end.")
@@ -385,23 +388,23 @@ def _format_disks(host: DeployHost, device: str) -> None:
         f"zpool create zroot -O acltype=posixacl -O xattr=sa -O compression=lz4 -O atime=off {root_part}"
     )
 
-    host.run(f"partprobe")
+    host.run("partprobe")
     host.run(f"mkfs.vfat {boot} -n NIXOS_BOOT")
 
     # setup zfs dataset
-    host.run(f"zfs create -o mountpoint=none zroot/root")
-    host.run(f"zfs create -o mountpoint=none zroot/docker")
-    host.run(f"zfs create -o mountpoint=legacy zroot/root/nixos")
-    host.run(f"zfs create -o mountpoint=legacy zroot/root/home")
+    host.run("zfs create -o mountpoint=none zroot/root")
+    host.run("zfs create -o mountpoint=none zroot/docker")
+    host.run("zfs create -o mountpoint=legacy zroot/root/nixos")
+    host.run("zfs create -o mountpoint=legacy zroot/root/home")
 
 
 def _mount_disks(host: DeployHost, device: str) -> None:
-    host.run(f"zpool import -af")
+    host.run("zpool import -af")
     # and finally mount
-    host.run(f"mount -t zfs zroot/root/nixos /mnt")
-    host.run(f"mkdir -p /mnt/home /mnt/boot")
-    host.run(f"mount -t zfs zroot/root/home /mnt/home")
-    host.run(f"mount /dev/disk/by-label/NIXOS_BOOT /mnt/boot")
+    host.run("mount -t zfs zroot/root/nixos /mnt")
+    host.run("mkdir -p /mnt/home /mnt/boot")
+    host.run("mount -t zfs zroot/root/home /mnt/home")
+    host.run("mount /dev/disk/by-label/NIXOS_BOOT /mnt/boot")
 
 
 @task
@@ -426,12 +429,19 @@ def mount_disks(c, hosts, disk=""):
 def decrypt_host_keys(c, host, tmpdir):
     os.mkdir(f"{tmpdir}/etc")
     os.mkdir(f"{tmpdir}/etc/ssh")
-    for keyname in [ "ssh_host_rsa_key", "ssh_host_rsa_key.pub", "ssh_host_ed25519_key", "ssh_host_ed25519_key.pub" ]:
+    for keyname in [
+        "ssh_host_rsa_key",
+        "ssh_host_rsa_key.pub",
+        "ssh_host_ed25519_key",
+        "ssh_host_ed25519_key.pub",
+    ]:
         if keyname.endswith(".pub"):
             os.umask(0o133)
         else:
             os.umask(0o177)
-        c.run(f"sops --extract '[\"{keyname}\"]' -d {ROOT}/hosts/{host}.yml > {tmpdir}/etc/ssh/{keyname}")
+        c.run(
+            f"sops --extract '[\"{keyname}\"]' -d {ROOT}/hosts/{host}.yml > {tmpdir}/etc/ssh/{keyname}"
+        )
 
 
 @task
@@ -442,25 +452,28 @@ def reformat_install_nixos(c, host, dhcp_interface):
     with tempfile.TemporaryDirectory() as tmpdir:
         decrypt_host_keys(c, host, tmpdir)
         # nixos_remote_pxe = "sudo nixos-remote-pxe"
-        nixos_remote_pxe = "sudo -E PYTHONPATH=$PYTHONPATH PATH=$PATH python3 /home/romano/Documents/tmp/nixos-anywhere/nixos-remote-pxe.py" # TODO nixos-remote-pxe needs packaging
-        c.run(f"{nixos_remote_pxe} --flake .#{host} --netboot-image-flake 'github:nix-community/nixos-images/pxe-boot#netboot-installer-nixos-unstable' --dhcp-interface {dhcp_interface} --extra-files {tmpdir} --no-reboot --pause-after-completion")
+        nixos_remote_pxe = "sudo -E PYTHONPATH=$PYTHONPATH PATH=$PATH python3 /home/romano/Documents/tmp/nixos-anywhere/nixos-remote-pxe.py"  # TODO nixos-remote-pxe needs packaging
+        c.run(
+            f"{nixos_remote_pxe} --flake .#{host} --netboot-image-flake 'github:nix-community/nixos-images/pxe-boot#netboot-installer-nixos-unstable' --dhcp-interface {dhcp_interface} --extra-files {tmpdir} --no-reboot --pause-after-completion"
+        )
     info("Device information:")
     info(
         "Remember to note down MAC addresses for IPMI port and network ports connected to foreign routers."
     )
     # TODO after starting nixos-remote-pxe, but before running nixos-remote (or
     # afterwards), we want to check if booted into uefi and:
-    #h.run("nix-shell -p inxi --command 'inxi -F'")
-    #h.run("nix-shell -p inxi --command 'inxi -FZ'")
-    #h.run("nix-shell -p ipmitool --command 'ipmitool lan print 1'")
-    #h.run("nix-shell -p ipmitool --command 'ipmitool lan print 2'")
-    #h.run("reboot")
+    # h.run("nix-shell -p inxi --command 'inxi -F'")
+    # h.run("nix-shell -p inxi --command 'inxi -FZ'")
+    # h.run("nix-shell -p ipmitool --command 'ipmitool lan print 1'")
+    # h.run("nix-shell -p ipmitool --command 'ipmitool lan print 2'")
+    # h.run("reboot")
 
 
 @task
 def print_tinc_key(c, hosts):
     for h in get_hosts(hosts):
         h.run("tinc.retiolum export")
+
 
 @task
 def print_age_key(c, host):
@@ -470,7 +483,16 @@ def print_age_key(c, host):
     import subprocess
 
     proc = subprocess.run(
-        ["sops", "--extract", '["ssh_host_ed25519_key.pub"]', "-d", f"{ROOT}/hosts/{host}.yml"], text=True, stdout=subprocess.PIPE, check=True
+        [
+            "sops",
+            "--extract",
+            '["ssh_host_ed25519_key.pub"]',
+            "-d",
+            f"{ROOT}/hosts/{host}.yml",
+        ],
+        text=True,
+        stdout=subprocess.PIPE,
+        check=True,
     )
     print("###### Age key ######")
     subprocess.run(
@@ -479,7 +501,6 @@ def print_age_key(c, host):
         check=True,
         text=True,
     )
-
 
 
 @task
@@ -492,24 +513,37 @@ def generate_ssh_cert(c, host):
     with tempfile.TemporaryDirectory() as tmpdir:
         # should we use ssh-keygen -A (Generate host keys of all default key tpyes) here?
         c.run(f"mkdir -p {tmpdir}/etc/ssh")
-        for keytype in [ "rsa", "ed25519" ]:
-             res = c.run(f"sops --extract '[\"ssh_host_{keytype}_key.pub\"]' -d {sops_file}", warn=True)
-             privkey = Path(f"{tmpdir}/etc/ssh/ssh_host_{keytype}_key")
-             pubkey = Path(f"{tmpdir}/etc/ssh/ssh_host_{keytype}_key.pub")
-             if len(res.stdout) == 0:
-                 # create host key with comment -c and empty passphrase -N ''
-                 c.run(f"ssh-keygen -f {privkey} -t {keytype} -C 'host key for host {host}' -N ''")
-                 c.run(f"sops --set '[\"ssh_host_{keytype}_key\"] {json.dumps(privkey.read_text())}' {sops_file}")
-                 c.run(f"sops --set '[\"ssh_host_{keytype}_key.pub\"] {json.dumps(pubkey.read_text())}' {sops_file}")
-             else:
-                 # save existing cert so we can generate an ssh certificate
-                 pubkey.write_text(res.stdout)
+        for keytype in ["rsa", "ed25519"]:
+            res = c.run(
+                f"sops --extract '[\"ssh_host_{keytype}_key.pub\"]' -d {sops_file}",
+                warn=True,
+            )
+            privkey = Path(f"{tmpdir}/etc/ssh/ssh_host_{keytype}_key")
+            pubkey = Path(f"{tmpdir}/etc/ssh/ssh_host_{keytype}_key.pub")
+            if len(res.stdout) == 0:
+                # create host key with comment -c and empty passphrase -N ''
+                c.run(
+                    f"ssh-keygen -f {privkey} -t {keytype} -C 'host key for host {host}' -N ''"
+                )
+                c.run(
+                    f"sops --set '[\"ssh_host_{keytype}_key\"] {json.dumps(privkey.read_text())}' {sops_file}"
+                )
+                c.run(
+                    f"sops --set '[\"ssh_host_{keytype}_key.pub\"] {json.dumps(pubkey.read_text())}' {sops_file}"
+                )
+            else:
+                # save existing cert so we can generate an ssh certificate
+                pubkey.write_text(res.stdout)
 
         os.umask(0o077)
-        c.run(f"sops --extract '[\"ssh-ca\"]' -d {ROOT}/modules/sshd/ca-keys.yml > {tmpdir}/ssh-ca")
+        c.run(
+            f"sops --extract '[\"ssh-ca\"]' -d {ROOT}/modules/sshd/ca-keys.yml > {tmpdir}/ssh-ca"
+        )
         valid_hostnames = f"{h}.r,{h}.dse.in.tum.de,{h}.thalheim.io"
         pubkey_path = f"{tmpdir}/etc/ssh/ssh_host_ed25519_key.pub"
-        c.run(f"ssh-keygen -h -s {tmpdir}/ssh-ca -n {valid_hostnames} -I {h} {pubkey_path}")
+        c.run(
+            f"ssh-keygen -h -s {tmpdir}/ssh-ca -n {valid_hostnames} -I {h} {pubkey_path}"
+        )
         signed_key_src = f"{tmpdir}/etc/ssh/ssh_host_ed25519_key-cert.pub"
         signed_key_dst = f"{ROOT}/modules/sshd/certs/{host}-cert.pub"
         c.run(f"mv {signed_key_src} {signed_key_dst}")
@@ -540,7 +574,7 @@ find {ROOT} \
 
 
 def wait_for_host(host: str, shutdown: bool = False) -> None:
-    import socket, time
+    import time
 
     while True:
         res = subprocess.run(
@@ -576,6 +610,7 @@ def generate_password(c, user="root"):
     print(f"{user}-password: {passw}")
     print(f"{user}-password-hash: {out.stdout}")
 
+
 @task
 def add_server(c, hostname):
     """
@@ -586,16 +621,15 @@ def add_server(c, hostname):
 
     print(f"Adding {hostname}")
 
-
     keys = None
-    with open(f"{ROOT}/pubkeys.json","r") as f:
+    with open(f"{ROOT}/pubkeys.json", "r") as f:
         keys = f.read()
     keys = json.loads(keys)
-    if(keys["machines"].get(hostname,None)):
+    if keys["machines"].get(hostname, None):
         print("Configuration already exists")
         exit(-1)
     keys["machines"][hostname] = ""
-    with open(f"{ROOT}/pubkeys.json","w") as f:
+    with open(f"{ROOT}/pubkeys.json", "w") as f:
         json.dump(keys, f, indent=2)
 
     update_sops_files(c)
@@ -606,33 +640,42 @@ def add_server(c, hostname):
     size = 12
     chars = string.ascii_letters + string.digits
     passwd = "".join(random.choice(chars) for x in range(size))
-    passwd_hash = subprocess.check_output(["mkpasswd", "-m", "sha-512", "-s"], input=passwd,text=True)
-    with open(sops_file,"w") as hosts:
+    passwd_hash = subprocess.check_output(
+        ["mkpasswd", "-m", "sha-512", "-s"], input=passwd, text=True
+    )
+    with open(sops_file, "w") as hosts:
         hosts.write(f"root-password: {passwd}\n")
         hosts.write(f"root-password-hash: {passwd_hash}")
-    enc_out = subprocess.check_output(["sops", "-e", f"{sops_file}"],text=True)
-    with open(sops_file,"w") as hosts:
+    enc_out = subprocess.check_output(["sops", "-e", f"{sops_file}"], text=True)
+    with open(sops_file, "w") as hosts:
         hosts.write(enc_out)
 
     print("Generating SSH certificate")
-    generate_ssh_cert(c,hostname)
+    generate_ssh_cert(c, hostname)
 
     print("Generating age key")
-    key_ed = subprocess.Popen(["sops", "--extract", '["ssh_host_ed25519_key.pub"]', "-d", sops_file], stdout=subprocess.PIPE)
-    
-    age = subprocess.check_output(["nix", "run", "--inputs-from", ".#", "nixpkgs#ssh-to-age"],text=True, stdin=key_ed.stdout)
+    key_ed = subprocess.Popen(
+        ["sops", "--extract", '["ssh_host_ed25519_key.pub"]', "-d", sops_file],
+        stdout=subprocess.PIPE,
+    )
+
+    age = subprocess.check_output(
+        ["nix", "run", "--inputs-from", ".#", "nixpkgs#ssh-to-age"],
+        text=True,
+        stdin=key_ed.stdout,
+    )
     age = age.rstrip()
 
     print("Updating pubkeys.json")
-    keys = None 
-    with open(f"{ROOT}/pubkeys.json","r") as f:
+    keys = None
+    with open(f"{ROOT}/pubkeys.json", "r") as f:
         keys = json.load(f)
     keys["machines"][hostname] = age
-    with open(f"{ROOT}/pubkeys.json","w") as f:
+    with open(f"{ROOT}/pubkeys.json", "w") as f:
         json.dump(keys, f, indent=2)
 
     print("Updating sops files")
-    update_sops_files(c) 
+    update_sops_files(c)
 
     example_host_config = f"""
 {{
@@ -645,16 +688,18 @@ def add_server(c, hostname):
   system.stateVersion = "22.11";
 }}"""
     print(f"Writing example hosts/{hostname}.nix")
-    with open(f"{ROOT}/hosts/{hostname}.nix","w") as f:
+    with open(f"{ROOT}/hosts/{hostname}.nix", "w") as f:
         f.write(example_host_config)
 
-    c.run(f"git add " + 
-          f"{ROOT}/hosts/{hostname}.nix " +
-          f"{ROOT}/hosts/{hostname}.yml " + 
-          f"{ROOT}/pubkeys.json " +
-          f"{ROOT}/.sops.yaml " +
-          f"{ROOT}/modules/secrets.yml " + 
-          f"{ROOT}/modules/sshd/certs/{hostname}-cert.pub")
+    c.run(
+        "git add "
+        + f"{ROOT}/hosts/{hostname}.nix "
+        + f"{ROOT}/hosts/{hostname}.yml "
+        + f"{ROOT}/pubkeys.json "
+        + f"{ROOT}/.sops.yaml "
+        + f"{ROOT}/modules/secrets.yml "
+        + f"{ROOT}/modules/sshd/certs/{hostname}-cert.pub"
+    )
 
 
 @task
