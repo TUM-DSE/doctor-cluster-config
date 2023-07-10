651b69d8f2d089b7b26efaa572aef46792102e86
Jörg Thalheim <joerg@thalheim.io>
2023-07-10 17:53:53 +0200

    k3s: update secrets

--- modules/k3s/server.nix
+++ modules/k3s/server.nix
@@ -1,6 +1,7 @@
 { ...
 }: {
   imports = [ ./. ];
+  sops.secrets.k3s-server-token.sopsFile = ./secrets.yml;
   networking.firewall.allowedTCPPorts = [ 6443 ];
   services.k3s.extraFlags = "--disable traefik --flannel-backend=host-gw --snapshotter=zfs --container-runtime-endpoint unix:///run/containerd/containerd.sock";
 }
