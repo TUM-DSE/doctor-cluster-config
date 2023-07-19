225b5080c3b8cf2dc5e400bd058c6992c79b95f6
Jörg Thalheim <joerg@thalheim.io>
2023-07-19 21:55:30 +0200

    asahi: replace overlay with import

--- modules/apple-silicon-support/packages/overlay.nix
+++ modules/apple-silicon-support/packages/overlay.nix
@@ -1,7 +1,9 @@
-final: prev: {
+final: prev: rec {
   linux-asahi = final.callPackage ./linux-asahi { };
   m1n1 = final.callPackage ./m1n1 { };
-  uboot-asahi = final.callPackage ./uboot-asahi { };
+  uboot-asahi = final.callPackage ./uboot-asahi {
+    inherit m1n1;
+  };
   asahi-fwextract = final.callPackage ./asahi-fwextract { };
   mesa-asahi-edge = final.callPackage ./mesa-asahi-edge { inherit (prev) mesa; };
   # TODO: package alsa-ucm-conf-asahi for headphone jack support
