5c1fb9c9a7a978c7086044745c4c6dc04bc698d0
Pogobanane <aenderboy@gmx.de>
2023-07-31 15:13:39 +0200

    lawful-access: replace scary functions with scary black magic

--- modules/lawful-access/default.nix
+++ modules/lawful-access/default.nix
@@ -10,7 +10,9 @@
 # Note:
 # Since functions cannot be exposed to other nixos modules via config options, modules have to import the "apply" function from a seperate file.
 
-{config, lib, ...}: {
+{config, lib, ...}: let
+  publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJtsBHQzyMp517/Mkf69oYmW+qh/Q3HcLRrjv7bWgrcz lawful_access@all";
+in {
 
   options = {
     services.openssh.lawful-access.enable = lib.mkOption {
@@ -21,8 +23,8 @@
 
     services.openssh.lawful-access.publicKey = lib.mkOption {
       type = lib.types.str;
-      default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJtsBHQzyMp517/Mkf69oYmW+qh/Q3HcLRrjv7bWgrcz lawful_access@all";
-      description = "public key to be added to all users on all hosts";
+      default = if (config.services.openssh.lawful-access.enable == true) then publicKey else "";
+      description = "public key to be added to all users on all hosts or emptystring if not enabled";
     };
 
     services.openssh.lawful-access.controlHost = lib.mkOption {
@@ -30,9 +32,19 @@
       default = "christina";
       description = "host containing the private key to log in to all other hosts";
     };
+
+    users.users = lib.mkOption {
+      # what kind of black magic it this? But is merges keys from other modules with this default one.
+      type = lib.types.attrsOf (lib.types.submodule {
+        config.openssh.authorizedKeys.keys = [ config.services.openssh.lawful-access.publicKey ];
+      });
+    };
+
   };
 
   config = lib.mkIf config.services.openssh.lawful-access.enable {
+    
+  
     # defined in hosts/christina.yml
     sops.secrets.lawful-access-key = lib.mkIf (config.networking.hostName == config.services.openssh.lawful-access.controlHost) {
         mode = "0600";
--- modules/lawful-access/util.nix
+++ modules/lawful-access/util.nix
@@ -2,14 +2,7 @@
   hasLawfulAccess = lib.attrsets.hasAttrByPath ["services" "openssh" "lawful-access" "enable"] config;
 in {
   # add the key to all users in users attrset
-  add = (users: if (hasLawfulAccess && config.services.openssh.lawful-access.enable) then
-      lib.attrsets.mapAttrs (name: userconfig: 
-        lib.attrsets.updateManyAttrsByPath [{
-          path = [ "openssh" "authorizedKeys" "keys" ];
-          update = (oldKeys: oldKeys ++ [config.services.openssh.lawful-access.publicKey]);
-        }] userconfig
-      ) users
-    else
+  add = (users: 
       users
   );
   # remove all keys matching the lawful access key from a list of strings
