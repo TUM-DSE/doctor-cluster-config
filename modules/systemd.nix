3dba40c6f5ff91f4e35c6a2f1633fbbaf351bfaf
Pogobanane <aenderboy@gmx.de>
2023-11-08 10:56:34 +0100

    fix failing ruby build

--- modules/systemd.nix
+++ modules/systemd.nix
@@ -1,9 +1,10 @@
-{
+{ pkgs, ... }: {
   # we don't really have services that need a long shutdown.
   systemd.extraConfig = ''
     DefaultTimeoutStopSec = 20s
   '';
 
   # allow scheduling of jobs at arbitrary times
-  services.atd.enable = true;
+  # (atd doesnt build for riscv64-linux)
+  services.atd.enable = !(pkgs.stdenv.hostPlatform == "riscv64-linux"); # if system != "riscv64-linux" then true else false;
 }
