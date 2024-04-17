80aa77070b585b6833494a72a89da8ee678ea6ea
Ilya Meignan--Masson <ilya.meignan-masson@mailoo.org>
2024-04-17 09:33:49 +0000

    allowing overcommitment of memory on irene

--- hosts/irene.nix
+++ hosts/irene.nix
@@ -4,6 +4,7 @@
     ../modules/hardware/supermicro-AS-2015CS.nix
     ../modules/nfs/client.nix
     ../modules/vfio/iommu-amd.nix
+    ../modules/dpdk.nix
   ];
 
   networking.hostName = "irene";
@@ -11,6 +12,7 @@
   disko.rootDisk = "/dev/disk/by-id/nvme-SAMSUNG_MZQL23T8HCLS-00A07_S64HNE0T804198";
   # Use the PCI 5.0 SSD for the experiments. This lines binds it automatically to vfio
   #virtualisation.vfio.devices = [ "1e0f:0013" ]; 
+  boot.kernel.sysctl = { "vm.overcommit_memory" = 1; };
 
   system.stateVersion = "22.11";
   simd.arch = "znver4";
