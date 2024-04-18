9107dfd49854d45a963c97c4dd44b646635d805f
Jörg Thalheim <joerg@thalheim.io>
2024-04-18 10:35:49 +0200

    fix aarch64 kernel build again

--- modules/zfs.nix
+++ modules/zfs.nix
@@ -1,13 +1,15 @@
-{ lib, pkgs, ... }: {
+{ config, lib, pkgs, ... }:
+{
   services.zfs = {
     autoSnapshot.enable = true;
     autoSnapshot.monthly = 1;
     autoScrub.enable = true;
   };
   networking.hostId = "8425e348";
-  boot.kernelPackages = lib.mkDefault (pkgs.zfsUnstable.override {
-    removeLinuxDRM = pkgs.hostPlatform.isAarch64;
-  }).latestCompatibleLinuxPackages;
+  boot.kernelPackages =
+    lib.mkDefault
+      (pkgs.zfsUnstable.override { removeLinuxDRM = pkgs.hostPlatform.isAarch64; })
+      .latestCompatibleLinuxPackages;
   boot.zfs.enableUnstable = true;
   boot.zfs.removeLinuxDRM = pkgs.hostPlatform.isAarch64;
 
@@ -15,4 +17,15 @@
   services.udev.extraRules = ''
     ACTION=="add|change", KERNEL=="sd[a-z]*[0-9]*|mmcblk[0-9]*p[0-9]*|nvme[0-9]*n[0-9]*p[0-9]*", ENV{ID_FS_TYPE}=="zfs_member", ATTR{../queue/scheduler}="none"
   '';
+
+  boot.kernelPatches =
+    lib.optional (config.boot.zfs.removeLinuxDRM && pkgs.stdenv.hostPlatform.system == "aarch64-linux")
+      {
+        name = "export-neon-symbols-as-gpl";
+        patch = pkgs.fetchpatch {
+          url = "https://github.com/torvalds/linux/commit/aaeca98456431a8d9382ecf48ac4843e252c07b3.patch";
+          hash = "sha256-L2g4G1tlWPIi/QRckMuHDcdWBcKpObSWSRTvbHRIwIk=";
+          revert = true;
+        };
+      };
 }
