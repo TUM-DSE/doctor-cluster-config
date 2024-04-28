13763432227a1bde3ffc7b5458f713c2842bbed1
Pogobanane <aenderboy@gmx.de>
2024-04-28 23:11:58 +0200

    tmp hotfix for bill and nardole

--- modules/hardware/supermicro-X12SCZ-TLN4F.nix
+++ modules/hardware/supermicro-X12SCZ-TLN4F.nix
@@ -1,10 +1,14 @@
 { lib
+, pkgs
 , modulesPath
 , ...
 }: {
   imports = [
     "${modulesPath}/installer/scan/not-detected.nix"
   ];
+
+  # Mellanox driver is broken in current 6.6.28 kernel. TODO switch back once we update nixpkgs to 6.6.29.
+  boot.kernelPackages = pkgs.linuxPackages_6_1;
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
