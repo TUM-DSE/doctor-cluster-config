7599c982e7101f2998985fb3412791b8fb1322ad
Jörg Thalheim <joerg@thalheim.io>
2024-06-11 10:17:20 +0200

    deploy container registry without container
    Than we don't need to fight against dns reflection.

--- modules/k3s/server.nix
+++ modules/k3s/server.nix
@@ -5,9 +5,6 @@
   networking.firewall.allowedTCPPorts = [ 6443 ];
   services.k3s.extraFlags = "--disable traefik --snapshotter=zfs --container-runtime-endpoint unix:///run/containerd/containerd.sock --flannel-backend=host-gw";
 
-  virtualisation.oci-containers.containers.registry-proxy = {
-    image = "registry";
-    environment.REGISTRY_PROXY_REMOTEURL = "https://registry-1.docker.io";
-    ports = [ "80:5000" ]; # use port 80, because this is what we whitelist in the firewall of the runner
-  };
+  services.dockerRegistry.enable = true;
+  services.dockerRegistry.extraConfig.registry.proxy.remoteurl = "https://registry-1.docker.io";
 }
