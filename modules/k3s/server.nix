b1f87a1152f0d65bc509845f4d792a406a19fa7e
Jörg Thalheim <joerg@thalheim.io>
2024-06-11 10:21:20 +0200

    container registry: change listen address to ::

--- modules/k3s/server.nix
+++ modules/k3s/server.nix
@@ -6,5 +6,6 @@
   services.k3s.extraFlags = "--disable traefik --snapshotter=zfs --container-runtime-endpoint unix:///run/containerd/containerd.sock --flannel-backend=host-gw";
 
   services.dockerRegistry.enable = true;
+  services.dockerRegistry.listenAddress = "[::]";
   services.dockerRegistry.extraConfig.registry.proxy.remoteurl = "https://registry-1.docker.io";
 }
