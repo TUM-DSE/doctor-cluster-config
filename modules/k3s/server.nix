cd52b24fd7e4cc1c762668105ec4230d398789fc
Jörg Thalheim <joerg@thalheim.io>
2024-06-11 09:51:11 +0200

    deploy registry-proxy to astrid

--- modules/k3s/server.nix
+++ modules/k3s/server.nix
@@ -4,4 +4,10 @@
   sops.secrets.k3s-server-token.sopsFile = ./secrets.yml;
   networking.firewall.allowedTCPPorts = [ 6443 ];
   services.k3s.extraFlags = "--disable traefik --snapshotter=zfs --container-runtime-endpoint unix:///run/containerd/containerd.sock --flannel-backend=host-gw";
+
+  virtualisation.oci-containers.containers.registry-proxy = {
+    image = "registry";
+    environment.REGISTRY_PROXY_REMOTEURL = "https://registry-1.docker.io";
+    ports = [ "5000:5000" ];
+  };
 }
