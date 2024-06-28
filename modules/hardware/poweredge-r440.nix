68c2b7dd11f86de514235a797fbb95ae24726e57
Jörg Thalheim <joerg@thalheim.io>
2024-06-28 11:04:27 +0200

    split of filesystems from r440 profile

--- modules/hardware/poweredge-r440.nix
+++ modules/hardware/poweredge-r440.nix
@@ -10,16 +10,6 @@
   boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "nvme" "megaraid_sas" ];
   boot.kernelModules = [ "kvm-intel" ];
 
-  fileSystems."/" = {
-    device = "zroot/root/nixos";
-    fsType = "zfs";
-  };
-
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-label/NIXOS_BOOT";
-    fsType = "vfat";
-    options = [ "nofail" ];
-  };
 
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
