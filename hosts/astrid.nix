5002984997cc40540ed998c426a948f3c2bf20ec
Jörg Thalheim <joerg@thalheim.io>
2024-06-28 12:40:15 +0200

    {astrid,dan,mickey}: migrate to disko

--- hosts/astrid.nix
+++ hosts/astrid.nix
@@ -3,9 +3,11 @@
     ../modules/k3s/server.nix
     ../modules/k3s/runner-monitoring.nix
     ../modules/hardware/poweredge-r440.nix
-    ../modules/filesystems-r440.nix
+    ../modules/disko-zfs.nix
     ../modules/nfs/client.nix
   ];
+
+  disko.rootDisk = "/dev/disk/by-id/nvme-Dell_Ent_NVMe_P5600_MU_U.2_1.6TB_PHAB123301411P9SGN";
 
   networking.hostName = "astrid";
 
--- hosts/dan.nix
+++ hosts/dan.nix
@@ -2,9 +2,10 @@
   imports = [
     ../modules/k3s/agent.nix
     ../modules/hardware/poweredge-r440.nix
-    ../modules/filesystems-r440.nix
+    ../modules/disko-zfs.nix
     ../modules/nfs/client.nix
   ];
+  disko.rootDisk = "/dev/disk/by-id/nvme-Dell_Ent_NVMe_P5600_MU_U.2_1.6TB_PHAB1234003F1P9SGN";
 
   networking.hostName = "dan";
 
