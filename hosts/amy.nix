eab198330ade1df988d992993a6a988bde4d8f43
Pogobanane <aenderboy@gmx.de>
2024-07-17 18:28:38 +0200

    copy vmux config from rose to amy

--- hosts/amy.nix
+++ hosts/amy.nix
@@ -8,6 +8,9 @@
     ../modules/xilinx.nix
     ../modules/xrdp.nix
     ../modules/xrdp-passwords.nix
+
+    ../modules/vfio/iommu-amd.nix
+    ../modules/dpdk.nix
   ];
 
   networking.hostName = "amy";
@@ -20,6 +23,29 @@
 
   simd.arch = "znver3";
   system.stateVersion = "22.11";
+
+  boot.hugepages1GB.number = 0;
+  # boot.hugepages2MB.number = 0;
+  boot.hugepages2MB.number = let
+    gb = 1500;
+  in gb * 1024 / 2;
+
+  # external deduplicating zfs for large numbers of VMs
+  # formating:
+  # create linux partition with fdisk
+  # sudo zpool create zokelmannvms -O acltype=posixacl -O xattr=sa -O compression=lz4 -O atime=off /dev/disk/by-partuuid/ced04b7c-f718-4997-8306-c33fb44a04e2
+  # sudo zfs create -o mountpoint=legacy zokelmannvms/vms
+  fileSystems."/scratch/okelmann/vmuxIO/VMs" = {
+    device = "zokelmannvms/vms";
+    fsType = "zfs";
+    # options = [ "uid=${builtins.toString config.users.users.okelmann.uid}" ];
+  };
+  systemd.services.set-vmount-owner = {
+    description = "Set ownership of /scratch/okelmann/vmuxIO/VMs";
+    after = [ "local-fs.target" ];
+    wantedBy = [ "multi-user.target" ];
+    script = "chown -R okelmann:users /scratch/okelmann/vmuxIO/VMs";
+  };
 
   # manually added to load xilinx from
   fileSystems."/share" = {
