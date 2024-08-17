ebf32ca1e107dd77b3f12b95dcd8957081d3b309
Jörg Thalheim <joerg@thalheim.io>
2024-08-17 13:45:41 +0200

    enable also comma integration

--- modules/nix-index.nix
+++ modules/nix-index.nix
@@ -1,4 +1,9 @@
-{ pkgs, ... }: {
+{ pkgs, inputs, ... }: {
+
+  imports = [
+    inputs.nix-index-database.nixosModules.nix-index
+  ];
   # we don't have a pre-built database for this platform
   programs.nix-index.enable = pkgs.stdenv.system != "riscv64-linux";
+  programs.nix-index-database.comma.enable = pkgs.stdenv.system != "riscv64-linux";
 }
