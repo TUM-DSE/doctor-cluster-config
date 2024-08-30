92a9947f2040ef9d103433c990c2b35a9e874eb0
Masanori Misono <m.misono760@gmail.com>
2024-08-30 11:21:59 +0000

    ian: enable tdx config

--- hosts/ian.nix
+++ hosts/ian.nix
@@ -3,6 +3,7 @@
   imports = [
     ../modules/hardware/poweredge-r760.nix
     ../modules/nfs/client.nix
+    ../modules/intel_tdx.nix
   ];
 
   disko.rootDisk = "/dev/disk/by-id/nvme-SAMSUNG_MZQL23T8HCLS-00A07_S64HNS0W800690";
---
+++ modules/intel_tdx.nix
@@ -0,0 +1,20 @@
+{ pkgs, lib, ... }:
+let
+  linux = pkgs.callPackage ../pkgs/kernels/linux-tdx.nix { };
+  linuxPackages = pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor linux);
+in
+{
+  # Configuration for Intel TDX
+
+  boot.kernelPackages = lib.mkForce linuxPackages;
+  boot.zfs.package = pkgs.zfsUnstable; # needed for 6.9
+
+  boot.kernelParams = [
+    "kvm_intel.tdx=1"
+    # TDX cannot survive from S3 and deeper states
+    "nohibernate"
+  ];
+
+  # enable libvirtd service
+  virtualisation.libvirtd.enable = true;
+}
