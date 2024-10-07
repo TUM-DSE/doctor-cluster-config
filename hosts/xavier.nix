82a86bbc11340f109c95bf76ee7dd28ddb310049
Jörg Thalheim <joerg@thalheim.io>
2024-10-07 13:04:06 +0200

    {ian,xavier}: set simd.arch

--- hosts/xavier.nix
+++ hosts/xavier.nix
@@ -6,6 +6,8 @@
 
   disko.rootDisk = "/dev/disk/by-id/nvme-SAMSUNG_MZQL23T8HCLS-00A07_S64HNN0W800552";
 
+  simd.arch = "sapphirerapids";
+
   networking.hostName = "xavier";
 
   system.stateVersion = "23.05";
