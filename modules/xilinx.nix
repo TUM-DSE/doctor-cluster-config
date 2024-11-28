088138b44506aacb36f567ceb198d0264276da08
Jörg Thalheim <joerg@thalheim.io>
2024-11-28 11:52:15 +0100

    remove xilinx user
    it's better if everyone is using their own account instead.

--- modules/xilinx.nix
+++ modules/xilinx.nix
@@ -42,39 +42,5 @@ in
     #  lib.mkIf (config.hardware.xilinx.xrt-drivers.enable) pkgs.linuxPackages_5_10;
 
     hardware.graphics.extraPackages = [ packages.xrt ];
-
-    # this is broken just now, we may need to update the firmware from here:
-    # https://packages.xilinx.com/ui/native/debian-packages/pool/
-    # systemd.services.setup-xilinx-firmware = {
-    #   wantedBy = [ "multi-user.target" ];
-    #   script = ''
-    #     set -x
-    #     rm -rf /lib/firmware/xilinx /opt/xilinx
-    #     mkdir -p /lib/firmware/xilinx /opt/xilinx/firmware
-    #     cp -r ${packages.xrt}/opt/xilinx/xrt /opt/xilinx/xrt
-    #     cp -r ${packages.xilinx-firmware}/lib/firmware/xilinx /lib/firmware/
-    #     cp -r ${packages.firmware-sn1000}/lib/firmware/xilinx/sn1000 /lib/firmware/xilinx/
-
-    #     cp -r ${packages.xilinx-firmware}/opt/xilinx/firmware /opt/xilinx
-    #     for p in ${packages.xilinx-firmware}/share/xilinx-firmware/*; do
-    #        echo $p
-    #        $p
-    #     done
-    #   '';
-    # };
-
-    sops.secrets.xilinx-password-hash.neededForUsers = true;
-
-    users.extraUsers.xilinx = {
-      isNormalUser = true;
-      hashedPasswordFile = lib.mkIf config.users.withSops config.sops.secrets.xilinx-password-hash.path;
-      extraGroups = [
-        "wheel"
-        "docker"
-        "plugdev"
-        "input"
-      ];
-      uid = 5002;
-    };
   };
 }
