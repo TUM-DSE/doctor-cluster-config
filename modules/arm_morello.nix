bd11580639e34736e461ecd2fd5d647c686930f4
Martin Fink <martin@finkmartin.com>
2024-11-29 09:36:05 +0000

    use pre-built perf-linux

--- modules/arm_morello.nix
+++ modules/arm_morello.nix
@@ -5,9 +5,9 @@
   ...
 }:
 let
-  linuxPackages = pkgs.recurseIntoAttrs (
-    pkgs.linuxPackagesFor self.packages.${pkgs.hostPlatform.system}.linux-morello
-  );
+  linuxPackages = (pkgs.linuxPackagesFor self.packages.${pkgs.hostPlatform.system}.linux-morello).extend(final: prev: {
+    perf = pkgs.linuxPackages.perf;
+  });
 in
 {
   boot.kernelPackages = lib.mkForce linuxPackages;
