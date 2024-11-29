d5506556ddfae698e934c8423d56f2ebf6bf0e3a
Martin Fink <martin@finkmartin.com>
2024-11-29 13:16:45 +0000

    nixfmt

--- modules/arm_morello.nix
+++ modules/arm_morello.nix
@@ -5,9 +5,9 @@
   ...
 }:
 let
-  linuxPackages = (pkgs.linuxPackagesFor self.packages.${pkgs.hostPlatform.system}.linux-morello).extend(final: prev: {
-    perf = pkgs.linuxPackages.perf;
-  });
+  linuxPackages =
+    (pkgs.linuxPackagesFor self.packages.${pkgs.hostPlatform.system}.linux-morello).extend
+      (final: prev: { perf = pkgs.linuxPackages.perf; });
 in
 {
   boot.kernelPackages = lib.mkForce linuxPackages;
