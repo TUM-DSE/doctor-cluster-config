786c656a18b3bd0192381e89658e31852a9f4241
Jörg Thalheim <joerg@thalheim.io>
2024-12-02 12:48:28 +0100

    jack: force eval

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -1,9 +1,9 @@
-{ config, ... }:
+{ config, lib, ... }:
 {
   hardware.opengl.enable = true;
   hardware.nvidia.datacenter.enable = true;
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.dc_535;
-  systemd.services.nvidia-fabricmanager.enable = false;
+  systemd.services.nvidia-fabricmanager.enable = lib.mkForce false;
 
   virtualisation.docker.enable = true;
   hardware.nvidia-container-toolkit.enable = true;
