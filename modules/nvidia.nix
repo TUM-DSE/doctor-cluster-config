9d09b4ee8402b065b1b9b087467cb81cd64613c8
Jörg Thalheim <joerg@thalheim.io>
2025-01-08 18:21:09 +0100

    migrate away from hardware.opengl

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -1,11 +1,11 @@
 { config, lib, ... }:
 {
-  hardware.opengl.enable = true;
+  hardware.graphics.enable = true;
   hardware.nvidia.datacenter.enable = true;
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.dc_535;
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.dc_565;
   systemd.services.nvidia-fabricmanager.enable = lib.mkForce false;
 
   virtualisation.docker.enable = true;
   hardware.nvidia-container-toolkit.enable = true;
-  hardware.opengl.driSupport32Bit = true;
+  hardware.graphics.enable32Bit = true;
 }
