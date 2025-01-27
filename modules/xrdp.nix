f99d34594f77da134d79a1278a55d29e6447d7e4
Jörg Thalheim <joerg@thalheim.io>
2025-01-27 09:20:19 +0100

    xrdp: fix broken fonts

--- modules/xrdp.nix
+++ modules/xrdp.nix
@@ -4,12 +4,14 @@
   services.xrdp.enable = true;
   # FIXME: this is actually ignored with the latest xrdp
   services.xrdp.defaultWindowManager = "${pkgs.xfce.xfce4-session}/bin/xfce4-session";
+
   services.xserver = {
     enable = true;
     desktopManager.xfce.enable = true;
     xkb.layout = "us";
     xkb.variant = "altgr-intl";
   };
+  fonts.fontconfig.enable = true;
   fonts.enableDefaultPackages = true;
 
   environment.etc."xrdp/startwm.sh" = {
