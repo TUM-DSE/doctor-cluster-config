07e7ff5315b36c4ac551c0ead0e48364ab74d076
Thore Sommer <mail@thson.de>
2025-04-26 11:26:28 +0200

    jamie: enable nvidia drivers, disable iommu

--- hosts/jamie.nix
+++ hosts/jamie.nix
@@ -1,9 +1,10 @@
 
-{
+{ config, ... }:{
   imports = [
     ../modules/hardware/poweredge7625.nix
     ../modules/nfs/client.nix
     ../modules/amd_sev_snp.nix
+    ../modules/nvidia.nix
   ];
 
   simd.arch = "znver4";
@@ -16,4 +17,12 @@
   networking.hostName = "jamie";
 
   system.stateVersion = "23.05";
+
+  boot.kernelParams = [ "amd_iommu=off" ];
+
+
+  # Additional Nvidia options
+  hardware.nvidia.gsp.enable = true;
+  hardware.firmware = [ config.boot.kernelPackages.nvidiaPackages.dc_565.firmware ];
+  hardware.nvidia.open = true;
 }
