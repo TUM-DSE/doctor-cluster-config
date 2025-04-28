3d9559ded73328dd128123d89f928c9c0348c2fd
Ilya Meignan--Masson <ilya.meignan-masson@mailoo.org>
2025-04-28 08:11:54 +0000

    adding Hristina G

--- modules/users/students.nix
+++ modules/users/students.nix
@@ -122,6 +122,10 @@ let
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCxVhUYcWx1CpHG78ae74qhOCB2YdBRDc3swxCp3R2tfzuE69ioRpDCej5L7rjL5lnT/aq1ra1GA73E2FcfWqMu3mjpCPSQPWgrYT+5U5GN301yME4Tkfc1IfsSC/6EgCUN6eRNLgSGnnRr2fGbuF1DetqGl4JX2bR5nh+7ub8ys/EfE0Y7NDWtYfJUjQtHn01KOUYLahQPU4D95QRj/o9UXifMAp/9GNEgPGAtMJTkQlVEHwJ95TT6C/qpgViYOzeZbSCJAokJYflHayC3RGCQmBTyhyv4+7LtkSEJQStDTa5HhLzubri9vCtmnS4wgmCql2MbmIVGnIgX8uS5x+cW+N1bXQECJVqT8S9J41dXEl5Z/pBFUhrdclpJkTbdTG2VZjnqHgarBWKnaATQXv0HOViHt4OX3KWR3YSZqSrN8mL5fwpSamlapxKk0c7AmJESeHGZWswBsTygx2o0Txq59HH5UKcBkikIX1pQtrTccdJ3ki4vD/sFmIWRVfsw+xd95zzO+zO/Np+BoaDSH26q7k5U39tyvgEhJPrAPVOLwH5jSKq9ZdluGx/q+cyGw8nuNgx1f13LyHr45X7HhqNdIxmGOqjUQvg5M2TFhehu15bWNpZNYfxR8gW2STxftuDlkEiYqxpBmNeP+RYdG+jCqECMlKvl8V9xLm8rXYzadQ== osmanyasar@w202-4u-v4.eduroam.dynamic.rbg.tum.de"
   ];
 
+  hristinagKeys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJfp4yvOGKPTqcdr9YZOJLZulCptWHK/4mV0IuIQ/Tvl hrisigr@gmail.com"
+  ];
+
   extraGroups = [
     "wheel"
     "docker"
@@ -575,6 +579,19 @@ in
         "yasmin"
       ];
       openssh.authorizedKeys.keys = osmanKeys;
+    };
+
+    # Hristina Grigorova, BSc thesis with Ilya (udos)
+    hristinag = {
+      isNormalUser = true;
+      home = "/home/hristinag";
+      inherit extraGroups;
+      shell = "/run/current-system/sw/bin/bash";
+      uid = 2073;
+      allowedHosts = [
+        "irene"
+      ];
+      openssh.authorizedKeys.keys = hristinagKeys;
     };
   };
 
