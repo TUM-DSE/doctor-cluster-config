45a7dadde70ae7d6c87b957bff11fc2934edd90d
Jörg Thalheim <joerg@thalheim.io>
2025-06-29 18:23:25 +0200

    factor out zokelmannvms into a module

--- hosts/amy.nix
+++ hosts/amy.nix
@@ -11,14 +11,18 @@
     ../modules/amd_sev_snp.nix
     ../modules/vfio/iommu-amd.nix
     ../modules/dpdk.nix
+    # ../modules/zokelmannvms.nix # ZFS pool import fails currently
   ];
 
   networking.hostName = "amy";
 
+  # Configuration for zokelmannvms module (when enabled)
+  # services.zokelmannvms.partitionUuid = "ced04b7c-f718-4997-8306-c33fb44a04e2";
+
   # for some reason the disk naming/usage of /dev/nvme{0,1}n1 is
   # inconsistent on this server compared to others. Lets now be
   # super explicit, in case this is due to inconsistent device
-  # naming by kernels. 
+  # naming by kernels.
   disko.rootDisk = "/dev/disk/by-id/nvme-SAMSUNG_MZQL21T9HCJR-00A07_S64GNA0T724989";
 
   simd.arch = "znver3";
@@ -33,23 +37,6 @@
     gb * 1024 / 2;
 
   networking.doctor-bridge.enable = true;
-
-  # external deduplicating zfs for large numbers of VMs
-  # formating:
-  # create linux partition with fdisk
-  # sudo zpool create zokelmannvms -O acltype=posixacl -O xattr=sa -O compression=lz4 -O atime=off /dev/disk/by-partuuid/ced04b7c-f718-4997-8306-c33fb44a04e2
-  # sudo zfs create -o mountpoint=legacy zokelmannvms/vms
-  fileSystems."/scratch/okelmann/vmuxIO/VMs" = {
-    device = "zokelmannvms/vms";
-    fsType = "zfs";
-    # options = [ "uid=${builtins.toString config.users.users.okelmann.uid}" ];
-  };
-  systemd.services.set-vmount-owner = {
-    description = "Set ownership of /scratch/okelmann/vmuxIO/VMs";
-    after = [ "local-fs.target" ];
-    wantedBy = [ "multi-user.target" ];
-    script = "chown -R okelmann:users /scratch/okelmann/vmuxIO/VMs";
-  };
 
   # manually added to load xilinx from
   fileSystems."/share" = {
