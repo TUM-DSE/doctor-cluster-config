58602684f0b16c4b0790e78d67f9b417b786978b
Jörg Thalheim <joerg@thalheim.io>
2025-09-30 15:59:20 +0200

    buildbot-nix: remove nixpkgs instance

--- flake.nix
+++ flake.nix
@@ -27,8 +27,7 @@
     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
 
     buildbot-nix.url = "github:nix-community/buildbot-nix";
-    # We need the nixpkgs unstable here.
-    #buildbot-nix.inputs.nixpkgs.follows = "nixpkgs";
+    buildbot-nix.inputs.nixpkgs.follows = "nixpkgs";
     buildbot-nix.inputs.flake-parts.follows = "flake-parts";
     buildbot-nix.inputs.treefmt-nix.follows = "treefmt-nix";
 
