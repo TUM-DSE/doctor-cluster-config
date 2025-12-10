5df2289edd1062b09a5dc91e0a1399db96e6e61a
Thore Sommer <thore.sommer@tum.de>
2025-12-10 13:02:33 +0100

    irene: disable IOMMU

--- hosts/irene.nix
+++ hosts/irene.nix
@@ -4,7 +4,6 @@
     ../modules/ipmi-supermicro.nix
     ../modules/hardware/supermicro-AS-2015CS.nix
     ../modules/nfs/client.nix
-    ../modules/vfio/iommu-amd.nix
     ../modules/dpdk.nix
     ../modules/elasticsearch.nix
     ../modules/amd_sev_snp-vanilla.nix
@@ -14,11 +13,31 @@
   # Use the Samsung SSD for the system
   disko.rootDisk = "/dev/disk/by-id/nvme-SAMSUNG_MZQL23T8HCLS-00A07_S64HNE0T804198";
   # Use the PCI 5.0 SSD for the experiments. This lines binds it automatically to vfio
-  #virtualisation.vfio.devices = [ "1e0f:0013" ]; 
+  #virtualisation.vfio.devices = [ "1e0f:0013" ];
   boot.kernel.sysctl = {
     "vm.overcommit_memory" = 1;
   };
   powerManagement.cpuFreqGovernor = "performance";
+
+  # IOMMU disabled, using unsafe no-IOMMU mode for VFIO
+  boot.kernelParams = [
+    "amd_iommu=off"
+    "pcie_aspm.policy=performance"
+  ];
+
+  # Load VFIO modules with specific parameters
+  boot.kernelModules = [
+    "vfio"
+    "vfio-pci"
+  ];
+
+  boot.extraModprobeConfig = ''
+    options vfio enable_unsafe_noiommu_mode=1
+    options vfio-pci disable_idle_d3=1
+  '';
+
+  # Blacklist vmd module
+  boot.blacklistedKernelModules = [ "vmd" ];
 
   system.stateVersion = "22.11";
   simd.arch = "znver4";
