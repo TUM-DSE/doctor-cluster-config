61a9bd73593b39c5509db9b01a4233f4ec35a6b5
Masanori Misono <m.misono760@gmail.com>
2025-12-28 17:16:01 +0000

    pkvm: enable ccache

--- modules/ccache.nix
+++ modules/ccache.nix
@@ -3,8 +3,35 @@
   programs.ccache = {
     enable = true;
     cacheDir = "/var/cache/ccache";
-    packageNames = [ "linux" ];
   };
 
   nix.settings.extra-sandbox-paths = [ config.programs.ccache.cacheDir ];
+  nixpkgs.overlays = [
+    (self: super: {
+      ccacheWrapper = super.ccacheWrapper.override {
+        extraConfig = ''
+          export CCACHE_DIR="${config.programs.ccache.cacheDir}"
+          export CCACHE_UMASK=000
+          export CCACHE_SLOPPINESS=random_seed
+          export CCACHE_MAXSIZE=20G
+          if [ ! -d "$CCACHE_DIR" ]; then
+            echo "====="
+            echo "Directory '$CCACHE_DIR' does not exist"
+            echo "Please create it with:"
+            echo "  sudo mkdir -m0770 '$CCACHE_DIR'"
+            echo "  sudo chown root:nixbld '$CCACHE_DIR'"
+            echo "====="
+            exit 1
+          fi
+          if [ ! -w "$CCACHE_DIR" ]; then
+            echo "====="
+            echo "Directory '$CCACHE_DIR' is not accessible for user $(whoami)"
+            echo "Please verify its access permissions"
+            echo "====="
+            exit 1
+          fi
+        '';
+      };
+    })
+  ];
 }
