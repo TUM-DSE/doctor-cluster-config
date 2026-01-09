027bda8c9dedae81bfa70b93c0a356730f5150aa
David Schall <david.schall@tum.de>
2026-01-09 16:16:23 +0100

    Delete Osman Yasar's user and SSH key entries
    Removed Osman Yasar's SSH keys and user configuration.
    closes #927

--- modules/users/students.nix
+++ modules/users/students.nix
@@ -20,10 +20,6 @@ let
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHO3xtv+H2/Tjad7iNhVkjEjqjmmCgeqRjwmFVorHVRj max@r"
   ];
 
-  osmanKeys = [
-    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCxVhUYcWx1CpHG78ae74qhOCB2YdBRDc3swxCp3R2tfzuE69ioRpDCej5L7rjL5lnT/aq1ra1GA73E2FcfWqMu3mjpCPSQPWgrYT+5U5GN301yME4Tkfc1IfsSC/6EgCUN6eRNLgSGnnRr2fGbuF1DetqGl4JX2bR5nh+7ub8ys/EfE0Y7NDWtYfJUjQtHn01KOUYLahQPU4D95QRj/o9UXifMAp/9GNEgPGAtMJTkQlVEHwJ95TT6C/qpgViYOzeZbSCJAokJYflHayC3RGCQmBTyhyv4+7LtkSEJQStDTa5HhLzubri9vCtmnS4wgmCql2MbmIVGnIgX8uS5x+cW+N1bXQECJVqT8S9J41dXEl5Z/pBFUhrdclpJkTbdTG2VZjnqHgarBWKnaATQXv0HOViHt4OX3KWR3YSZqSrN8mL5fwpSamlapxKk0c7AmJESeHGZWswBsTygx2o0Txq59HH5UKcBkikIX1pQtrTccdJ3ki4vD/sFmIWRVfsw+xd95zzO+zO/Np+BoaDSH26q7k5U39tyvgEhJPrAPVOLwH5jSKq9ZdluGx/q+cyGw8nuNgx1f13LyHr45X7HhqNdIxmGOqjUQvg5M2TFhehu15bWNpZNYfxR8gW2STxftuDlkEiYqxpBmNeP+RYdG+jCqECMlKvl8V9xLm8rXYzadQ== osmanyasar@w202-4u-v4.eduroam.dynamic.rbg.tum.de"
-  ];
-
   christianKKeys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFfke0RoVMEtj0XjxdjatQgz0RQxs7gaGSgM2OoGBemO chris@deskpin"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL7ghrJVl24UkfnyNOz21jbmrnPImp3+UR4/p2xymbnl chris@deskpin"
@@ -179,24 +175,6 @@ in
       ];
       openssh.authorizedKeys.keys = maxKeys;
       expires = "2027-12-31";
-    };
-
-    # Osman Yasar, HiWi working for David on gem5-related implemenations
-    osman = {
-      isNormalUser = true;
-      home = "/home/osman";
-      inherit extraGroups;
-      shell = "/run/current-system/sw/bin/bash";
-      uid = 2072;
-      allowedHosts = [
-        "xavier"
-        "jamie"
-        "yasmin"
-        "ace"
-        "eliza"
-      ];
-      openssh.authorizedKeys.keys = osmanKeys;
-      expires = "2025-12-31";
     };
 
     # Christian Krinitsin, GR student with Theofilos.
@@ -421,6 +399,7 @@ in
     "moritz"
     "mwerndle"
     "nicola"
+    "osman"
     "paul"
     "paulz"
     "philip"
