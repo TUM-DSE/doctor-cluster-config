f1d30c4d08a651e4199d2a4c35a86a365eb47904
Patrick Sabanic <patrick.sabanic@tum.de>
2026-01-12 16:09:30 +0000

    Update kernel on jamie

--- pkgs/kernels/linux-svsm-host.nix
+++ pkgs/kernels/linux-svsm-host.nix
@@ -39,7 +39,8 @@ let
     modDirVersionArg = "6.5.0";
     extraPatches = [ ];
   };
-  coconut_svsm_with_restricted_interrupt = { # untested
+  coconut_svsm_with_restricted_interrupt = {
+    # untested
     owner = "Sabanic-P";
     repo = "linux";
     rev = "8f3e2540e2131e0559ef2b008387cc715ed9d505"; # branch restricted_injection
@@ -70,56 +71,75 @@ let
   };
 
   coconut_svsm_6_11 = {
-	owner = "coconut-svsm";
-	repo = "linux";
-	rev = "3b04c21f46a75560b83bf07eb233f1b414a65324"; # branch svsm
-	sha256 = "sha256-nr/DfNX+u0KosVcE6sTbAHeD/hOP4jQpuTjdfUSFFOg=";
-	version = "6.11";
-	modDirVersionArg = "6.11.0";
-	extraPatches = [ ];
+    owner = "coconut-svsm";
+    repo = "linux";
+    rev = "3b04c21f46a75560b83bf07eb233f1b414a65324"; # branch svsm
+    sha256 = "sha256-nr/DfNX+u0KosVcE6sTbAHeD/hOP4jQpuTjdfUSFFOg=";
+    version = "6.11";
+    modDirVersionArg = "6.11.0";
+    extraPatches = [ ];
   };
 
   coconut_svsm_6_11_latest = {
-	owner = "coconut-svsm";
-	repo = "linux";
-	rev = "3d7f4e43fb9b1b41f1f0ef09c8f6d770505e59ce"; # branch svsm
-	sha256 = "sha256-GCwpZswt8AH6vG28d812CvCC2cVZqIKj0Y+KgVq7pOI=";
-	version = "6.11";
-	modDirVersionArg = "6.11.0";
-	extraPatches = [ ];
+    owner = "coconut-svsm";
+    repo = "linux";
+    rev = "3d7f4e43fb9b1b41f1f0ef09c8f6d770505e59ce"; # branch svsm
+    sha256 = "sha256-GCwpZswt8AH6vG28d812CvCC2cVZqIKj0Y+KgVq7pOI=";
+    version = "6.11";
+    modDirVersionArg = "6.11.0";
+    extraPatches = [ ];
+  };
+  coconut_svsm_6_11_latest_vc = {
+    owner = "sabtf";
+    repo = "linux";
+    rev = "4fdd6b98e4ff627b80460a0a6fe96c902c46d015"; # branch svsm
+    sha256 = "sha256-QYkL8AfG8f0uLfNB8NcpJvPKRCGetivWHGFVehf0XWI=";
+    version = "6.11-vc";
+    modDirVersionArg = "6.11.0";
+    extraPatches = [ ];
   };
 
   # snp_kernel = svsm_preview_hv_1;
-  snp_kernel = coconut_svsm_6_11_latest;
+  snp_kernel = coconut_svsm_6_11_latest_vc;
 in
 with snp_kernel;
-buildLinux (args // rec {
-  inherit version;
-  modDirVersion =
-    if (snp_kernel.modDirVersionArg == null) then
-      builtins.replaceStrings [ "-" ] [ ".0-" ] version
-    else
-      modDirVersionArg;
-  src = fetchFromGitHub {
-    inherit owner repo rev sha256;
-  };
+buildLinux (
+  args
+  // rec {
+    inherit version;
+    modDirVersion =
+      if (snp_kernel.modDirVersionArg == null) then
+        builtins.replaceStrings [ "-" ] [ ".0-" ] version
+      else
+        modDirVersionArg;
+    src = fetchFromGitHub {
+      inherit
+        owner
+        repo
+        rev
+        sha256
+        ;
+    };
 
-  kernelPatches = [
-    {
-      name = "amd_sme-config";
-      patch = null;
-      extraConfig = ''
-        AMD_MEM_ENCRYPT y
-        CRYPTO_DEV_CCP y
-        CRYPTO_DEV_CCP_DD y
-        CRYPTO_DEV_SP_PSP y
-        KVM_AMD_SEV y
-        MEMORY_FAILURE y
-        EXPERT y
-        VFIO_DEVICE_CDEV y
-      '';
-    }
-  ] ++ extraPatches;
-  extraMeta.branch = version;
-  ignoreConfigErrors = true;
-} // (args.argsOverride or { }))
+    kernelPatches = [
+      {
+        name = "amd_sme-config";
+        patch = null;
+        extraConfig = ''
+          AMD_MEM_ENCRYPT y
+          CRYPTO_DEV_CCP y
+          CRYPTO_DEV_CCP_DD y
+          CRYPTO_DEV_SP_PSP y
+          KVM_AMD_SEV y
+          MEMORY_FAILURE y
+          EXPERT y
+          VFIO_DEVICE_CDEV y
+        '';
+      }
+    ]
+    ++ extraPatches;
+    extraMeta.branch = version;
+    ignoreConfigErrors = true;
+  }
+  // (args.argsOverride or { })
+)
