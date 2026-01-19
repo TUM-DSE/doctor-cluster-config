0744671812e395cab2d6bcb521d3c26f6d9db2b5
Pogobanane <aenderboy@gmx.de>
2026-01-19 23:23:49 +0100

    fixup frontend deployment

--- modules/lrz-gitlab-classroom/frontend.nix
+++ modules/lrz-gitlab-classroom/frontend.nix
@@ -9,7 +9,7 @@ in {
 
     # locations."/".proxyPass = "https://pogobanane.de/";
     locations."/" = {
-      proxyPass = "https://localhost:8017/"; # TODO !
+      proxyPass = "http://unix:/run/lrz-gitlab-classroom-frontend.sock";
       extraConfig = ''
         proxy_http_version 1.1;
 		    proxy_set_header Upgrade $http_upgrade;
@@ -35,29 +35,69 @@ in {
   sops.secrets.lrz-gitlab-classroom-tumonline-oauth-client-secret.owner = user;
   sops.secrets.lrz-gitlab-classroom-secret.owner = user;
 
+  systemd.sockets.lrz-gitlab-classroom-frontend = {
+    description = "LRZ GitLab Classroom Frontend Socket";
+    wantedBy = [ "sockets.target" ];
+    socketConfig = {
+      Accept = false;
+      ListenStream = "/run/lrz-gitlab-classroom-frontend.sock";
+      SocketUser = config.services.nginx.user;
+      SocketGroup = config.services.nginx.group;
+    };
+  };
+
   systemd.services.lrz-gitlab-classroom-frontend = {
     description = "LRZ GitLab Classroom Frontend";
-    wantedBy = [ "multi-user.target" ];
-    after = [ "network.target" ];
+    after = [ "network.target" "lrz-gitlab-classroom-frontend.socket" ];
+    requires = [ "lrz-gitlab-classroom-frontend.socket" ];
     environment = {
       TUMONLINE_OAUTH_CLIENT_ID = "";
       GITLAB_OAUTH_CLIENT_ID = "";
       GITLAB_OAUTH_SCOPE = "read_user";
       STATE_DIRECTORY = "/var/lib/lrz-gitlab-classroom";
-      TEMPLATE_PATH = "";
+      TEMPLATE_PATH = "${frontend}/share/templates/grnvs/";
       # Add non-secret env vars here
     };
     script = ''
-      mkdir -p /var/lib/lrz-gitlab-classroom || echo "Failed to create state directory /var/lib/lrz-gitlab-classroom"
       export GITLAB_OAUTH_CLIENT_SECRET="$(cat ${config.sops.secrets.lrz-gitlab-classroom-gitlab-oauth-client-secret.path})"
       export TUMONLINE_OAUTH_CLIENT_SECRET="$(cat ${config.sops.secrets.lrz-gitlab-classroom-tumonline-oauth-client-secret.path})"
       export SECRET="$(cat ${config.sops.secrets.lrz-gitlab-classroom-secret.path})"
-      exec ${frontend}/bin/lrz-gitlab-classroom-frontend daemon --proxy -l http://*?fd=3
+      exec ${frontend}/bin/lrz-gitlab-classroom-frontend daemon -m production --proxy -l http://*?fd=3
     '';
     serviceConfig = {
+      Type = "simple";
       Restart = "on-failure";
       User = "lrz-gitlab-teacher";
       Group = "lrz-gitlab-teacher";
+      StateDirectory = "lrz-gitlab-classroom"; # defaults to /var/lib/lrz-gitlab-classroom
     };
+
+    # Maybe the following restrictions?
+
+    # # make "systemd-analyze security" happier
+    # CapabilityBoundingSet=
+    # LockPersonality=yes
+    # MemoryDenyWriteExecute=yes
+    # NoNewPrivileges=yes
+    # PrivateDevices=yes
+    # PrivateTmp=yes
+    # ProcSubset=pid
+    # ProtectClock=yes
+    # ProtectControlGroups=yes
+    # ProtectHome=yes
+    # ProtectHostname=yes
+    # ProtectKernelLogs=yes
+    # ProtectKernelModules=yes
+    # ProtectKernelTunables=yes
+    # ProtectProc=invisible
+    # ProtectSystem=full
+    # RemoveIPC=yes
+    # RestrictAddressFamilies=AF_PACKET
+    # RestrictNamespaces=yes
+    # RestrictRealtime=yes
+    # RestrictSUIDSGID=yes
+    # SystemCallArchitectures=native
+    # SystemCallFilter=~@clock @cpu-emulation @debug @module @mount @obsolete @privileged @raw-io @reboot @resources @swap
+
   };
 }
