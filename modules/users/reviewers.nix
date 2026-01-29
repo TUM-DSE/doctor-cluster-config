0dbbf1b6b6e60204b39f5441fb34d0aff717106a
Anatole Lefort <anatole.lefort@gmail.com>
2026-01-29 08:11:14 +0100

    reviewers: remove vcxlgenRev{1..3} (asplos'26)

--- modules/users/reviewers.nix
+++ modules/users/reviewers.nix
@@ -11,15 +11,6 @@ let
   nsdiRev4 = [
     "ecdsa-sha2-nistp384 AAAAE2VjZHNhLXNoYTItbmlzdHAzODQAAAAIbmlzdHAzODQAAABhBDpbkn4baRZJNdP7LVRzYznjWipd3xN0s3378AlNkym7o8sTlukkwlXxVLiL9NApGllAFsDgE+Gd6Q13Oi/KPeARNvS0Ay9XPVR2s1GRM14HcWHtiOaIY5u5dnDUGPqcYA== nsdi-aec-reviewer"
   ];
-  vcxlgenRev1 = [
-    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDZ+H61SEboXJvsB07Hyj002kos3ANkxAIErBTysqbWdsXp3uxbqt8x2k9jxmXsEj29atsbmY/97m6R15SpnXeTNetehI2hq6DGlDY9LJQovyWAVna8OKaH2Zq1Iwn562HVVQP3dlbwAFHaleWjpT7YU7va15tOxSSzaBYWeZK0iMV9AaHol9516fuaueEjYU2DKaF7/4r8ACtrNzi9b/BNHnXg+xJf4eyosTgjfgU8r+K9fxCXp4pswilvWuGFNU4Xl2MjSNkqURcUOMcS+CosKzH+Dd4PBG+TQD7iwU/ImA+WcS/J+PrZNApGiPyQpE6t9A8anke8eih6831m5O1N vcxlgen-ae1"
-  ];
-  vcxlgenRev2 = [
-    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCyhLfOc5KlnzXXEU0qMr1G1iUVEX66nHByQLZfE89iuOJFm+QemesVeDen6T5bK8wPRuLFisjWEQZNGU+LqKoxsmaaKRtRAQyswL71ZYx4u/UgXsR7h4DhT0KMVtOS3pdJRMX5ZPW8jo4qDR+h9S2CmEOG8bzEyCeqMgVo0boybVN+99aR8pCirBAhXjmVjbHzIkV6m3bvJ7R03Ar/5g3KAN26/bG/BLoyi/MAYL8WioLuew3zKhrmP+njK+7qx7EcHgqPsp6ph0TrcXfFiwuoYzdmdNzinRrqwDTN4gEOWluIuIxFLZxI80tLWlaAQEOtj/03tBYR2lXuIwiVd7P/ vcxlgen-ae2"
-  ];
-  vcxlgenRev3 = [
-    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDgzscgO1YgCQJla9K/RsvAOtWujALbBSnSXp40HeW50zr5+CuPVbSN6Oh/ZzyXm8vtDjme6nzklNC2MgwI/db4PaMdy17SykjW92pAvDg9zFw8yP5iXQPhQEGtLfUoVjl1uXJlZhDSPC1YrBgCHbZCUl3lmL8zZ7B5LEQY4ZihV9tzCb4wC/+jt8J1gs7k4c7TFp7tOzlpZ7u5a2ZAH8i0ijgaVRUsyaGEGe8QBVGaZ4pbDaZmVojVp/9qH/yJS9d18ZF7t84C0lLpFxFNjMc10Cz5WKg5TH+nogxDZRTxJFklH4gynH+/eNqOHnlyTbo4y5I80RdeyKDr7qkktZ63 vcxlgen-ae3"
-  ];
 
   aranciniRev1 = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP/RD4fEVQ5lDFu8UDXtBx7HBTcyiUQQAW3qOSfgJZLQ asplos"
@@ -83,36 +74,6 @@ in {
       uid = 4203;
       allowedHosts = [ "ryan" "graham" ];
       openssh.authorizedKeys.keys = nsdiRev4;
-    };
-    # vCXLGen ASPLOS AE reviewer 1
-    vcxlgenRev1 = {
-      isNormalUser = true;
-      home = "/scratch/vcxlgenRev1";
-      inherit extraGroups;
-      shell = "/run/current-system/sw/bin/bash";
-      uid = 4300;
-      allowedHosts = [ "xavier" ];
-      openssh.authorizedKeys.keys = vcxlgenRev1;
-    };
-    # vCXLGen ASPLOS AE reviewer 2
-    vcxlgenRev2 = {
-      isNormalUser = true;
-      home = "/scratch/vcxlgenRev2";
-      inherit extraGroups;
-      shell = "/run/current-system/sw/bin/bash";
-      uid = 4303;
-      allowedHosts = [ "xavier" ];
-      openssh.authorizedKeys.keys = vcxlgenRev2;
-    };
-    # vCXLGen ASPLOS AE reviewer 3
-    vcxlgenRev3 = {
-      isNormalUser = true;
-      home = "/scratch/vcxlgenRev3";
-      inherit extraGroups;
-      shell = "/run/current-system/sw/bin/bash";
-      uid = 4304;
-      allowedHosts = [ "xavier" ];
-      openssh.authorizedKeys.keys = vcxlgenRev3;
     };
     # Arancini ASPLOS AE reviewer 1
     aranciniRev1 = {
@@ -191,5 +152,8 @@ in {
     "conextRev1"
     "conextRev2"
     "conextRev3"
+    "vcxlgenRev1"
+    "vcxlgenRev2"
+    "vcxlgenRev3"
   ];
 }
