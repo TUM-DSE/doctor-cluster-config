bd75d7aa233a0727148f1a2aa64aa3f9b20f0d6d
Thore Sommer <thore.sommer@tum.de>
2026-04-23 13:05:16 +0200

    polly: add nvidia module

--- hosts/polly.nix
+++ hosts/polly.nix
@@ -7,6 +7,7 @@
     ../modules/hardware/gigabyte-xv23-zx0.nix
     ../modules/nfs/client.nix
     ../modules/amd_sev_snp-vanilla.nix
+    ../modules/nvidia.nix
   ];
 
   disko.rootDisk = "/dev/disk/by-id/nvme-SAMSUNG_MZQL23T8HCLS-00A07_S64HNS0WA06423";
