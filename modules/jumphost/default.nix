44584af2964c23d31cf6841d3c3128d3a90e140c
Martin Fink <martin@finkmartin.com>
2026-05-21 13:47:15 +0000

    change hostname back to login.dos

--- modules/jumphost/default.nix
+++ modules/jumphost/default.nix
@@ -8,8 +8,8 @@
   # defined in astrid's secrets
   sops.secrets.deploy-ssh-key = { };
 
-  programs.ssh.knownHosts."dosvm3.cit.tum.de" = {
-    hostNames = [ "dosvm3.cit.tum.de" ];
+  programs.ssh.knownHosts."login.dos.cit.tum.de" = {
+    hostNames = [ "login.dos.cit.tum.de" ];
     publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJiLBCN41dSG/45o5/h/56VF02nRVWe2ssTbE7VrxZOA";
   };
 
@@ -22,10 +22,10 @@
       authorizedKeys = pkgs.writeText "authorized-keys" (builtins.concatStringsSep "\n" sshKeys);
     in
     {
-      description = "Update authorized keys on dosvm3.cit.tum.de";
+      description = "Update authorized keys on login.dos.cit.tum.de";
       wantedBy = [ "multi-user.target" ];
       script = ''
-        ${pkgs.openssh}/bin/ssh -T -v -i $CREDENTIALS_DIRECTORY/deploy-keys deploy-keys@dosvm3.cit.tum.de < ${authorizedKeys}
+        ${pkgs.openssh}/bin/ssh -T -v -i $CREDENTIALS_DIRECTORY/deploy-keys deploy-keys@login.dos.cit.tum.de < ${authorizedKeys}
       '';
       serviceConfig = {
         Type = "oneshot";
