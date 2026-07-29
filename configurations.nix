106bb97ab397e00602c4e66af6096cf906dac8aa
Jörg Thalheim <joerg@thalheim.io>
2026-07-29 22:56:18 +0200

    enable zfs-dedup on all machines

--- configurations.nix
+++ configurations.nix
@@ -27,6 +27,7 @@ let
     ./modules/zsh.nix
     ./modules/systemd.nix
     ./modules/cleanup.nix
+    ./modules/zfs-dedup.nix
     ./modules/tinc.nix
     ./modules/sshd
     ./modules/register-flake.nix
