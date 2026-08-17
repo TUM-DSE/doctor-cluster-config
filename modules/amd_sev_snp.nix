d1fd95c07a969da0078fc8d775ec9d966ce59725
Masanori Misono <m.misono760@gmail.com>
2026-08-17 11:20:12 +0000

    cleanup snp/tdx kernel config
    Signed-off-by: Masanori Misono <m.misono760@gmail.com>

--- modules/amd_sev_snp.nix
+++ modules/amd_sev_snp.nix
@@ -1,27 +1,26 @@
 { pkgs, lib, ... }:
-let
-  linux = pkgs.callPackage ../pkgs/kernels/linux-sev-snp.nix { };
-  linuxPackages = lib.recurseIntoAttrs (
-    (pkgs.linuxPackagesFor linux).extend (
-      _final: prev: {
-        # Kernel modules inherit the kernel's gcc13 stdenv (see
-        # linux-sev-snp.nix), but sysdig's userland half links
-        # gcc15-built libraries (onetbb), which needs the matching
-        # libstdc++. The kmod part still gets -std=gnu11 from Kbuild.
-        sysdig = prev.sysdig.override { stdenv = pkgs.stdenv; };
-      }
-    )
-  );
-in
 {
-  # Configuration for AMD SEV-SNP with AMD versions' kernel
-
-  boot.kernelPackages = lib.mkForce linuxPackages;
+  # Use 6.16 (or newer) kernel
+  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
   boot.zfs.package = pkgs.zfs_unstable; # needed for 6.9
+
+  boot.kernelPatches = [
+      {
+        name = "amd_sme-config";
+        patch = null;
+        extraConfig = ''
+          AMD_MEM_ENCRYPT y
+          CRYPTO_DEV_CCP y
+          CRYPTO_DEV_CCP_DD m
+          CRYPTO_DEV_SP_PSP y
+          KVM_AMD_SEV y
+          VFIO_DEVICE_CDEV y
+        '';
+      }
+  ]; 
 
   boot.kernelParams = [
     #"mem_encrypt=on"
-    "amd_iommu=off"
     "kvm_amd.sev=1"
     "kvm_amd.sev_es=1"
     "kvm_amd.sev_snp=1"
