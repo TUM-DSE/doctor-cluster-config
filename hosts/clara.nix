ee045432f60e66286f72e1e49e36922b6084bac8
halalboro <f20200947@pilani.bits-pilani.ac.in>
2026-08-25 13:13:45 +0000

    Enable grafana dashboard

--- hosts/clara.nix
+++ hosts/clara.nix
@@ -9,7 +9,7 @@
     ../modules/amd_sev_snp.nix
     ../modules/xilinx.nix
     ../modules/xrdp.nix
-    # ../modules/monitoring/fpga-dashboard # see email to admins from 13.08.2026
+    ../modules/monitoring/fpga-dashboard
   ];
 
   networking.hostName = "clara";
